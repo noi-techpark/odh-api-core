a204a286982abbc8fef531f39460137891429a8a
Rudolf Thoeni <rudolf.thoeni@idm-suedtirol.com>
2019-03-05 10:37:46 +0100

    added test method

---
+++ OdhApiCore/Controllers/api/Test.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdhApiCore.Controllers.api
+{
+    public class Test : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public Test(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet, Route("api/test")]
+        public string Get()
+        {
+            var x = configuration.GetConnectionString("PGConnection");
+
+            return x;
+        }
+    }
+}
