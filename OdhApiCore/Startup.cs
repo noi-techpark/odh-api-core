ee378f671c17ac166cf01f585217e8efe026f522
Tobias Burger <tobias_burger@hotmail.com>
2019-07-15 20:09:07 +0200

    Use new IWebHostEnvironment interface.

--- OdhApiCore/Startup.cs
+++ OdhApiCore/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.Hosting;
 
 namespace OdhApiCore
 {
@@ -71,7 +72,7 @@ namespace OdhApiCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
