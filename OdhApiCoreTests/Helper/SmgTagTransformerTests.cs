c3fe61f328cee48dab41eb9aec13891312d77e80
Tobias Burger <tobias_burger@hotmail.com>
2019-09-07 19:13:21 +0200

    Add SmgTagTransformer tests

---
+++ OdhApiCoreTests/Helper/SmgTagTransformerTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Helper;
+
+namespace OdhApiCoreTests.Helper
+{
+    public class SmgTagTransformerTests
+    {
+        public static TheoryData<string> GetLanguages()
+        {
+            return new TheoryData<string>
+            {
+                "de", "it", "en", "ru"
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetLanguages))]
+        public void TransformToLocalizedSmgTag_EmptySmgTags(string language)
+        {
+            var smgtagslist = Enumerable.Empty<SmgTags>();
+            IEnumerable<SmgTags> result = SmgTagTransformer.TransformToLocalizedSmgTag(
+                smgtagslist, language);
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetLanguages))]
+        public void TransformToLocalizedSmgTag_SingleSmgTag(string language)
+        {
+            var expectedSmgTag = new SmgTags
+            {
+                Id = Guid.NewGuid().ToString(),
+                Shortname = "shortname",
+                TagName = new Dictionary<string, string>
+                {
+                    { "de", "tagname" },
+                    { "it", "tagname" }
+                },
+                ValidForEntity = new string[] { }
+            };
+            var smgtagslist = new SmgTags[]
+            {
+                expectedSmgTag
+            };
+            IEnumerable<SmgTags> result = SmgTagTransformer.TransformToLocalizedSmgTag(
+                smgtagslist, language);
+            Assert.NotNull(result);
+            var smgtag = Assert.Single(result);
+            Assert.Equal(expectedSmgTag.Id, smgtag.Id);
+            Assert.Equal(expectedSmgTag.Shortname, smgtag.Shortname);
+            Assert.Equal(expectedSmgTag.ValidForEntity, smgtag.ValidForEntity);
+            if (expectedSmgTag.TagName.ContainsKey(language))
+                Assert.Single(smgtag.TagName);
+            else
+                Assert.Empty(smgtag.TagName);
+        }
+    }
+}
