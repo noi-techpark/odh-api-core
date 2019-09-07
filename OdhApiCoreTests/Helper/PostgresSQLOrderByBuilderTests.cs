f55e6fdc1cf4459e24058db5dcf41cb1c9bc3bad
Tobias Burger <tobias_burger@hotmail.com>
2019-09-07 16:26:33 +0200

    Add PostgresSQLOrderByBuilder tests

---
+++ OdhApiCoreTests/Helper/PostgresSQLOrderByBuilderTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Helper;
+
+namespace OdhApiCoreTests.Helper
+{
+    public class PostgresSQLOrderByBuilderTests
+    {
+        [Fact]
+        public void BuildSeedOrderBy_WithSeed()
+        {
+            string orderby = "";
+            PostgresSQLOrderByBuilder.BuildSeedOrderBy(ref orderby, "100", "");
+            Assert.Equal("md5(id || '10')", orderby);
+
+            orderby = "";
+            PostgresSQLOrderByBuilder.BuildSeedOrderBy(ref orderby, "42", "");
+            Assert.Equal("md5(id || '42')", orderby);
+
+            orderby = "";
+            PostgresSQLOrderByBuilder.BuildSeedOrderBy(ref orderby, "83", "");
+            Assert.Equal("md5(id || '8')", orderby);
+        }
+
+        [Fact]
+        public void BuildSeedOrderBy_WithoutSeed()
+        {
+            string orderby = "";
+            PostgresSQLOrderByBuilder.BuildSeedOrderBy(ref orderby, null, "orderbyclause");
+            Assert.Equal("orderbyclause", orderby);
+        }
+
+        [Fact]
+        public void BuildSeedOrderBy_WithInvalidSeed()
+        {
+            string orderby = "";
+            PostgresSQLOrderByBuilder.BuildSeedOrderBy(ref orderby, "invalidnumber", "orderbyclause");
+            // CHECK: Is this the correct behavior?
+            Assert.Equal("md5(id || '')", orderby);
+        }
+    }
+}
