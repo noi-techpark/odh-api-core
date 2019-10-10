2cc7bf0732cebe945fcb9d63f2fd75d3fb022d3e
Tobias Burger <tobias_burger@hotmail.com>
2019-10-10 15:20:24 +0200

    Move classes to separate files

---
+++ OdhApiCore/Controllers/base/NullStringParameterActionFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+
+namespace OdhApiCore.Controllers
+{
+    public class NullStringParameterActionFilterAttribute : ActionFilterAttribute
+    {
+        public NullStringParameterActionFilterAttribute()
+        {
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            foreach (var key in context.ActionArguments.Keys.ToArray())
+            {
+                var value = context.ActionArguments[key];
+                if (value as string == "null")
+                {
+                    context.ActionArguments[key] = null;
+                }
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}
--- OdhApiCore/Controllers/base/OdhController.cs
+++ OdhApiCore/Controllers/base/OdhController.cs
@@ -1,47 +1,12 @@
 ﻿using Helper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
 using System;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OdhApiCore.Controllers
 {
-    public class NullStringParameterActionFilterAttribute : ActionFilterAttribute
-    {
-        public NullStringParameterActionFilterAttribute()
-        {
-        }
-
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            foreach (var key in context.ActionArguments.Keys.ToArray())
-            {
-                var value = context.ActionArguments[key];
-                if (value as string == "null")
-                {
-                    context.ActionArguments[key] = null;
-                }
-            }
-            base.OnActionExecuting(context);
-        }
-    }
-
-    public class CommaSeparatedArrayBinder : Microsoft.AspNetCore.Mvc.ModelBinding.IModelBinder
-    {
-        public Task BindModelAsync(Microsoft.AspNetCore.Mvc.ModelBinding.ModelBindingContext bindingContext)
-        {
-            var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
-            var value = valueProviderResult.FirstValue; // get the value as string
-
-            var model = value.Split(",");
-            bindingContext.Result = Microsoft.AspNetCore.Mvc.ModelBinding.ModelBindingResult.Success(model);
-
-            return Task.CompletedTask;
-        }
-    }
 
     [ApiController]
     public abstract class OdhController : ControllerBase
