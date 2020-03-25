915753f84ce7569df4e4b9c885b78b998a2c7cea
Tobias Burger <tobias_burger@hotmail.com>
2020-03-25 19:39:08 +0100

    Add a PascalCaseJsonFormatter

--- OdhApiCore/Controllers/api/ActivityApiController.cs
+++ OdhApiCore/Controllers/api/ActivityApiController.cs
@@ -55,6 +55,7 @@ namespace OdhApiCore.Controllers
         /// <response code="200">List created</response>
         /// <response code="400">Request Error</response>
         /// <response code="500">Internal Server Error</response>
+        [Filters.PascalCaseJsonFormatter]
         [ProducesResponseType(typeof(IEnumerable<GBLTSActivity>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
