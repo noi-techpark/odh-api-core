eb1d5f4e53544340b3253d5769b872b7ffc321b4
Tobias Burger <tobias_burger@hotmail.com>
2020-03-27 10:28:42 +0100

    Fix paramters in unit tests

--- OdhApiCoreTests/Helper/PostgresSQLWhereBuilderTests.cs
+++ OdhApiCoreTests/Helper/PostgresSQLWhereBuilderTests.cs
@@ -78,12 +78,12 @@ namespace OdhApiCoreTests.Helper
             bool? highlight = null;
             bool? activefilter = null;
             bool? smgactivefilter = null;
+            string? searchfilter = null;
+            string? language = null;
             string? lastchange = null;
             var (where, parameters) =
                 PostgresSQLWhereBuilder.CreateActivityWhereExpression(
-                    idlist, activitytypelist, suttypelist, difficultylist, smgtaglist, distictlist, municipalitylist,
-                    tourismvereinlist, regionlist, arealist, distance, distancemin, distancemax, duration, durationmin,
-                    durationmax, altitude, altitudemin, altitudemax, highlight, activefilter, smgactivefilter, lastchange);
+                    idlist, activitytypelist, suttypelist, difficultylist, smgtaglist, distictlist, municipalitylist, tourismvereinlist, regionlist, arealist, distance, distancemin, distancemax, duration, durationmin, durationmax, altitude, altitudemin, altitudemax, highlight, activefilter, smgactivefilter, searchfilter, language, lastchange);
             Assert.Equal("", where);
             Assert.Empty(parameters);
         }
@@ -121,12 +121,15 @@ namespace OdhApiCoreTests.Helper
             bool? highlight = true;
             bool? activefilter = null;
             bool? smgactivefilter = null;
+            string? searchfilter = null;
+            string? language = null;
             string? lastchange = null;
             var (where, parameters) =
                 PostgresSQLWhereBuilder.CreateActivityWhereExpression(
                     idlist, activitytypelist, suttypelist, difficultylist, smgtaglist, distictlist, municipalitylist,
                     tourismvereinlist, regionlist, arealist, distance, distancemin, distancemax, duration, durationmin,
-                    durationmax, altitude, altitudemin, altitudemax, highlight, activefilter, smgactivefilter, lastchange);
+                    durationmax, altitude, altitudemin, altitudemax, highlight, activefilter, smgactivefilter,
+                    searchfilter, language, lastchange);
 
             Assert.Equal("id in (@id1, @id2) AND data @> @regionid AND (data @> @area1) AND (data @> @type1) AND data @> @highlight", where);
 
