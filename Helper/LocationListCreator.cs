18be4a8dfcea9002674892d625ddca2e0963a80d
Tobias Burger <tobias_burger@hotmail.com>
2020-04-16 13:25:26 +0200

    Remove unused methods

--- Helper/LocationListCreator.cs
+++ Helper/LocationListCreator.cs
@@ -10,65 +10,6 @@ namespace Helper
     public static class LocationListCreator
     {
         #region PostGres
-        [System.Obsolete]
-
-        public static async Task<List<string>> CreateActivityAreaListPGAsync(string areafilter, IPostGreSQLConnectionFactory connectionFactory, CancellationToken cancellationToken)
-        {
-            List<string> thearealist = new List<string>();
-
-            if (areafilter != null)
-            {
-
-                if (areafilter.Substring(areafilter.Length - 1, 1) == ",")
-                    areafilter = areafilter.Substring(0, areafilter.Length - 1);
-
-                //Klaub asanond
-                var splittedlocfilter = areafilter.Split(',');
-
-                foreach (string theareafilter in splittedlocfilter)
-                {
-                    string areatype = theareafilter.Substring(0, 3);
-
-                    switch (areatype)
-                    {
-                        case "reg":
-
-                            //Suche alle zugehörigen Areas für die Region
-                            thearealist.AddRange(await GetAreaforRegionPGAsync(theareafilter.Replace("reg", ""), connectionFactory, cancellationToken));
-
-                            break;
-
-                        case "tvs":
-
-                            //Suche alle zugehörigen TVs für die Region
-                            thearealist.AddRange(await GetAreaforTourismvereinPGAsync(theareafilter.Replace("tvs", ""), connectionFactory, cancellationToken));
-
-                            break;
-
-                        case "skr":
-
-                            //Suche alle zugehörigen TVs für die Region
-                            thearealist.AddRange(await GetAreaforSkiRegionPGAsync(theareafilter.Replace("skr", ""), connectionFactory, cancellationToken));
-
-                            break;
-
-                        case "ska":
-
-                            //Suche alle zugehörigen TVs für die Region
-                            thearealist.AddRange(await GetAreaforSkiAreaPGAsync(theareafilter.Replace("ska", ""), connectionFactory, cancellationToken));
-
-                            break;
-
-                        case "are":
-                            thearealist.Add(theareafilter.Replace("are", ""));
-                            break;
-                    }
-                }
-            }
-
-            return thearealist;
-        }
-
         public static async Task<IEnumerable<string>> CreateActivityAreaListPGAsync(QueryFactory queryFactory, string areafilter, CancellationToken cancellationToken)
         {
             if (areafilter == null)
@@ -134,32 +75,6 @@ namespace Helper
             return thearealist.Distinct();
         }
 
-        [System.Obsolete]
-        public static async Task<List<string>> GetAreaforRegionPGAsync(string regionId, IPostGreSQLConnectionFactory connectionFactory, CancellationToken cancellationToken)
-        {
-            List<string> arealist = new List<string>();
-
-            string select = "Id,data ->> 'Id'";
-            string where = "data ->> 'RegionId' = '" + regionId.ToUpper() + "'";
-
-            var result = PostgresSQLHelper.SelectFromTableDataAsStringAsync(connectionFactory, "areas", select, where, "", 0, null, cancellationToken);
-
-            await foreach (var area in result)
-            {
-                var areacodes = area.Value.Replace(" ", "").Replace("[", "").Replace("]", "").Replace("\"", "");
-
-                List<string> areacodeslist = areacodes.Split(',').ToList();
-
-                foreach (var areacode in areacodeslist)
-                {
-                    if (!arealist.Contains(areacode))
-                        arealist.Add(areacode);
-                }
-            }
-
-            return arealist;
-        }
-
         public static async Task<IEnumerable<string>> GetAreaforRegionPGAsync(this Query query, string regionId, CancellationToken cancellationToken)
         {
             return await
@@ -169,32 +84,6 @@ namespace Helper
                      .GetAsync<string>();
         }
 
-        [System.Obsolete]
-        public static async Task<List<string>> GetAreaforTourismvereinPGAsync(string tvId, IPostGreSQLConnectionFactory connectionFactory, CancellationToken cancellationToken)
-        {
-            List<string> arealist = new List<string>();
-
-            string select = "Id,data ->> 'Id'";
-            string where = "data ->> 'TourismvereinId' = '" + tvId.ToUpper() + "'";
-
-            var result = PostgresSQLHelper.SelectFromTableDataAsStringAsync(connectionFactory, "areas", select, where, "", 0, null, cancellationToken);
-
-            await foreach (var area in result)
-            {
-                var areacodes = area.Value.Replace(" ", "").Replace("[", "").Replace("]", "").Replace("\"", "");
-
-                List<string> areacodeslist = areacodes.Split(',').ToList();
-
-                foreach (var areacode in areacodeslist)
-                {
-                    if (!arealist.Contains(areacode))
-                        arealist.Add(areacode);
-                }
-            }
-
-            return arealist;
-        }
-
         public static async Task<IEnumerable<string>> GetAreaforTourismvereinPGAsync(this Query query, string tvId, CancellationToken cancellationToken)
         {
             return await
@@ -202,32 +91,6 @@ namespace Helper
                      .SelectRaw("data->>'Id'")
                      .WhereRaw("data->>'TourismvereinId' = ?", tvId.ToUpper())
                      .GetAsync<string>();
-        }
-
-        [System.Obsolete]
-        public static async Task<IEnumerable<string>> GetAreaforSkiRegionPGAsync(string skiregId, IPostGreSQLConnectionFactory connectionFactory, CancellationToken cancellationToken)
-        {
-            List<string> arealist = new List<string>();
-
-            string select = "Id,data ->> 'AreaId'";
-            string where = "data ->> 'SkiRegionId' = '" + skiregId.ToUpper() + "'";
-
-            var result = PostgresSQLHelper.SelectFromTableDataAsStringAsync(connectionFactory, "skiareas", select, where, "", 0, null, cancellationToken);
-
-            await foreach (var skiarea in result)
-            {
-                var areacodes = skiarea.Value.Replace(" ", "").Replace("[", "").Replace("]", "").Replace("\"", "");
-
-                List<string> areacodeslist = areacodes.Split(',').ToList();
-
-                foreach (var areacode in areacodeslist)
-                {
-                    if (!arealist.Contains(areacode))
-                        arealist.Add(areacode);
-                }
-            }
-
-            return arealist;
         }
 
         private static string[] FromJsonArray(string jsonArray) =>
@@ -242,26 +105,6 @@ namespace Helper
                      .GetAsync<string>();
             return areaIdsJson.SelectMany(FromJsonArray)
                               .Distinct();
-        }
-
-        [System.Obsolete]
-        public static async Task<IEnumerable<string>> GetAreaforSkiAreaPGAsync(string skiareaId, IPostGreSQLConnectionFactory connectionFactory, CancellationToken cancellationToken)
-        {
-            List<string> arealist = new List<string>();
-
-            //string select = "Id,data ->> 'AreaId'";
-            string select = "Id,data ->> 'AreaId'";
-            string where = "Id = '" + skiareaId.ToUpper() + "'";
-
-            var rawResult = await PostgresSQLHelper.SelectFromTableDataAsStringAsync(connectionFactory, "skiareas", select, where, "", 0, null, cancellationToken).SingleAsync();
-
-            var result = rawResult.Value;
-
-            result = result.Replace(" ", "").Replace("[", "").Replace("]", "").Replace("\"", "");
-
-            arealist = result.Split(',').ToList();
-
-            return arealist;
         }
 
         public static async Task<IEnumerable<string>> GetAreaforSkiAreaPGAsync(this Query query, string skiareaId, CancellationToken cancellationToken)
