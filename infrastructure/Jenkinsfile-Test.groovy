2651ae3cdcf75d2b191ec851f76f166da7c1a868
Alex Lanz <alex@krumer.it>
2020-06-29 13:54:09 +0200

    Adjusting deployment

---
+++ infrastructure/Jenkinsfile-Test.groovy
@@ -0,0 +1,50 @@
+pipeline {
+    agent any
+
+    environment {
+        DOCKER_PROJECT_NAME = "odh-tourism-api"
+        DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/odh-tourism-api'
+        DOCKER_TAG = "test-$BUILD_NUMBER"
+
+		SERVER_PORT = "1011"
+        
+        PG_CONNECTION = credentials('odh-tourism-api-test-pg-connection')
+    }
+
+    stages {
+        stage('Configure') {
+            steps {
+                sh """
+                    rm -f .env
+                    cp .env.example .env
+                    echo 'COMPOSE_PROJECT_NAME=${DOCKER_PROJECT_NAME}' >> .env
+                    echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
+                    echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
+
+					echo 'SERVER_PORT=${SERVER_PORT}' >> .env
+                    
+                    echo 'PG_CONNECTION=${PG_CONNECTION}' >> .env
+                """
+            }
+        }
+        stage('Build') {
+            steps {
+                sh '''
+                    aws ecr get-login --region eu-west-1 --no-include-email | bash
+                    docker-compose --no-ansi -f docker-compose.yml build --pull
+                    docker-compose --no-ansi -f docker-compose.yml push
+                '''
+            }
+        }
+        stage('Deploy') {
+            steps {
+               sshagent(['jenkins-ssh-key']) {
+                    sh """
+                        (cd infrastructure/ansible && ansible-galaxy install -f -r requirements.yml)
+                        (cd infrastructure/ansible && ansible-playbook --limit=test deploy.yml --extra-vars "release_name=${BUILD_NUMBER}")
+                    """
+                }
+            }
+        }
+    }
+}
