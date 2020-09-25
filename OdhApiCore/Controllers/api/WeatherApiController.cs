130ad6047b92b6caa4a3df41a2da7e5e342e1943
Rudolf Thoeni <rudolf.thoeni@idm-suedtirol.com>
2020-09-25 11:16:49 +0200

    adeding Weather api controller

---
+++ OdhApiCore/Controllers/api/WeatherApiController.cs
@@ -0,0 +1,30 @@
+﻿using DataModel;
+using Helper;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OdhApiCore.Responses;
+using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OdhApiCore.Controllers
+{
+    /// <summary>
+    /// WebcamInfo Api (data provided by LTS ActivityData) SOME DATA Available as OPENDATA
+    /// </summary>
+    [EnableCors("CorsPolicy")]
+    [NullStringParameterActionFilter]
+    public class WeatherApiController : OdhController
+    {        
+        public WeatherApiController(IWebHostEnvironment env, ISettings settings, ILogger<ActivityController> logger, QueryFactory queryFactory)
+            : base(env, settings, logger, queryFactory)
+        {
+        }   
+    }
+}
