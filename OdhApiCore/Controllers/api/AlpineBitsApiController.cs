736445e63b45ea78511233fad7f0b58780c021ab
Rudolf Thoeni <rudolf.thoeni@idm-suedtirol.com>
2020-09-30 08:57:41 +0200

    adding alpinebitscontroller

---
+++ OdhApiCore/Controllers/api/AlpineBitsApiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OdhApiCore.Controllers.api
+{
+    [EnableCors("CorsPolicy")]
+    [NullStringParameterActionFilter]
+    public class AlpineBitsApiController : OdhController
+    {
+
+    }
+}
