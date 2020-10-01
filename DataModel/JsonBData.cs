8aa770aa2e1f57c033a600eff0fe824406be4643
Tobias Burger <tobias_burger@hotmail.com>
2020-10-01 10:09:15 +0200

    Set nullable reference types in the project settings to a reasonable value

--- DataModel/JsonBData.cs
+++ DataModel/JsonBData.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace DataModel
 {
     public class JsonBData
     {
         public string id { get; set; }
-        public string data { get; set; }
+        public JToken data { get; set; }
     }
 }
