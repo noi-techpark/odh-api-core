4fdf0cfda095300b63105c74080465d02749a149
Tobias Burger <tobias_burger@hotmail.com>
2020-11-26 15:02:55 +0100

    Add a transformer for the usage from C#

---
+++ RawQueryParser/Transformer.fs
@@ -0,0 +1,15 @@
+﻿module RawQueryParser.Transformer
+
+open FParsec
+
+exception ParserException of string
+
+[<CompiledName "TransformSort">]
+let transformSort input =
+    match run Parser.statements input with
+    | Success (statements, _, _) ->
+        statements
+        |> Writer.writeStatements
+    | Failure (msg, _, _) ->
+        raise (ParserException msg)
+
