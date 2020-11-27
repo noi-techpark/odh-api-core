60230143ec3c47fa76f326ac3d69fb8c072b1a62
Rudolf Thoeni <rudolf.thoeni@idm-suedtirol.com>
2020-11-27 16:50:12 +0100

    forwardedheaders config

--- OdhApiCore/Startup.cs
+++ OdhApiCore/Startup.cs
@@ -275,7 +275,11 @@ namespace OdhApiCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseForwardedHeaders();
+            //app.UseForwardedHeaders();
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedProto
+            });
 
             if (env.IsDevelopment())
             {
