a27e11c058e5b977c53792f3416472fd89fcebb1
Rudi Thoeni <rudi.thoeni@hotmail.com>
2020-12-16 21:43:27 +0100

    adding file delete

--- OdhApiCore/Controllers/api/FileUploadController.cs
+++ OdhApiCore/Controllers/api/FileUploadController.cs
@@ -34,7 +34,7 @@ namespace OdhApiCore.Controllers.api
         }
 
         [ApiExplorerSettings(IgnoreApi = true)]
-        //[Authorize(Roles = "DataWriter,DataModify,DataCreate,ODHPoiCreate,ODHPoiModify,ODHPoiManager,CommonCreate,CommonModify,CommonManager,ArticleCreate,ArticleModify,ArticleManager,EventShortManager,EventShortCreate")]
+        [Authorize(Roles = "DataWriter,DataModify,DataCreate,ODHPoiCreate,ODHPoiModify,ODHPoiManager,CommonCreate,CommonModify,CommonManager,ArticleCreate,ArticleModify,ArticleManager,EventShortManager,EventShortCreate")]
         [HttpPost, Route("api/FileUpload/{type}/{directory}")]
         public async Task<IActionResult> PostFormData(string type, string directory, IFormCollection form)
         {
@@ -60,14 +60,51 @@ namespace OdhApiCore.Controllers.api
                     InputStream = file.OpenReadStream()
                 };
                 var response = await client.UploadPartAsync(request);
-                filenames.Add(String.Format("{0},{1}", settings.S3ImageresizerConfig.Url, filename));
+                filenames.Add(String.Format("{0}{1}", settings.S3ImageresizerConfig.Url, filename));
             }
             return Ok(filenames);
         }
 
-        //TODO have a look here https://weblog.west-wind.com/posts/2017/sep/14/accepting-raw-request-body-content-in-aspnet-core-api-controllers
-        //TODO UPload on S3 https://stackoverflow.com/questions/56813075/aws-c-sharp-net-core-how-to-upload-a-jpg-image-to-s3-bucket-without-saving-i
-        //TODO https://docs.aws.amazon.com/sdkfornet1/latest/apidocs/html/T_Amazon_S3_Model_PutObjectRequest.htm
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [Authorize(Roles = "DataWriter,DataMofify,DataCreate,DataDelete,ODHPoiCreate,ODHPoiModify,ODHPoiManager,ODHPoiUpdate,CommonCreate,CommonModify,CommonManager,CommonDelete,ArticleCreate,ArticleModify,ArticleManager,ArticleDelete")]
+        [HttpDelete, Route("api/FileDelete/{filepath}")]
+        public async Task<IActionResult> Delete(string filepath)
+        {
+            string keyName = filepath.Replace("|", "\\").Replace("$", ".");
+
+            // read from settings
+            var keyid = settings.S3ImageresizerConfig.AccessKey;
+            var key = settings.S3ImageresizerConfig.SecretKey;
+            var bucketName = settings.S3ImageresizerConfig.BucketAccessPoint;
+
+            var creds = new BasicAWSCredentials(keyid, key);
+            var config = new AmazonS3Config();
+            config.RegionEndpoint = RegionEndpoint.EUWest1;
+            var client = new AmazonS3Client(creds, config);
+
+            try
+            {
+                var deleteObjectRequest = new DeleteObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+
+                Console.WriteLine("Deleting an object");
+                await client.DeleteObjectAsync(deleteObjectRequest);
+
+                return Ok();
+            }
+            catch (AmazonS3Exception e)
+            {
+                return BadRequest(String.Format("Error encountered on server.Message:'{0}' when deleting an object", e.Message));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(String.Format("Unknown encountered on server. Message:'{0}' when deleting an object", e.Message));
+            }
+        }
+
 
         //[ApiExplorerSettings(IgnoreApi = true)]
         //[Authorize(Roles = "DataWriter,DataModify,DataCreate,ODHPoiCreate,ODHPoiModify,ODHPoiManager,CommonCreate,CommonModify,CommonManager,ArticleCreate,ArticleModify,ArticleManager,EventShortManager,EventShortCreate")]
@@ -180,42 +217,8 @@ namespace OdhApiCore.Controllers.api
 
         //}
 
-        //[ApiExplorerSettings(IgnoreApi = true)]
-        //[Authorize(Roles = "DataWriter,DataMofify,DataCreate,DataDelete,ODHPoiCreate,ODHPoiModify,ODHPoiManager,ODHPoiUpdate,CommonCreate,CommonModify,CommonManager,CommonDelete,ArticleCreate,ArticleModify,ArticleManager,ArticleDelete")]
-        //[HttpDelete, Route("api/FileDelete/{filepath}")]
-        //public IActionResult Delete(string filepath)
-        //{
-        //    string root = System.Configuration.ConfigurationManager.AppSettings["imageresizerpath"] + filepath.Replace("|", "\\").Replace("$", ".");
 
-        //    try
-        //    {
-        //        File.Delete(root);
-        //        return Ok();
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        return BadRequest(e.Message);
-        //    }
-        //}
     }
 
-    //TODO: https://stackoverflow.com/questions/46866849/multipartformdatastreamprovider-for-asp-net-core-2
 
-    //public class CustomMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
-    //{
-    //    public CustomMultipartFormDataStreamProvider(string path)
-    //        : base(path)
-    //    { }
-
-    //    public override string GetLocalFileName(System.Net.Http.Headers.HttpContentHeaders headers)
-    //    {
-    //        var fileExtension = Path.GetExtension(headers.ContentDisposition.FileName.Replace("\"", string.Empty));
-
-    //        Guid filenameguid = Guid.NewGuid();
-
-    //        var name = filenameguid.ToString() + fileExtension;
-
-    //        return name;
-    //    }
-    //}
 }
