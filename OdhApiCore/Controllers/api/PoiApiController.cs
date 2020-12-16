8f89440a15d47a0b3aa836b47553b33ccd56f150
Tobias Burger <tobias_burger@hotmail.com>
2020-12-16 13:57:08 +0100

    Fix response type for POI

--- OdhApiCore/Controllers/api/PoiApiController.cs
+++ OdhApiCore/Controllers/api/PoiApiController.cs
@@ -52,7 +52,7 @@ namespace OdhApiCore.Controllers.api
         /// <response code="200">List created</response>
         /// <response code="400">Request Error</response>
         /// <response code="500">Internal Server Error</response>
-        [ProducesResponseType(typeof(IEnumerable<GBLTSPoi>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(JsonResult<GBLTSPoi>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpGet, Route("Poi")]
