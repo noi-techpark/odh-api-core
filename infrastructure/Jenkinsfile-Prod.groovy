a935263bd874542af0b380ab40d073b4767ca709
RudiThoeni <30471361+RudiThoeni@users.noreply.github.com>
2021-01-19 15:01:31 +0100

    Update Jenkinsfile-Prod.groovy

--- infrastructure/Jenkinsfile-Prod.groovy
+++ infrastructure/Jenkinsfile-Prod.groovy
@@ -20,7 +20,7 @@ pipeline {
 	S3_BUCKET_ACCESSPOINT = credentials('odh-tourism-api-test-bucket-accesspoint')
 	S3_IMAGEUPLOADER_ACCESSKEY = credentials('odh-tourism-api-test-s3-imageuploader-accesskey')
 	S3_IMAGEUPLOADER_SECRETKEY = credentials('odh-tourism-api-test-s3-imageuploader-secretkey')
-	OAUTH_AUTORITY = credentials('odh-tourism-api-prod-oauth-authority')
+	OAUTH_AUTORITY = "https://auth.opendatahub.bz.it/auth/realms/noi/"
     }
 
     stages {
