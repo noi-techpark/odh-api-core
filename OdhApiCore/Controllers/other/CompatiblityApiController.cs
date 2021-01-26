71e8ab1255c9a51a8abbf96e4c2443217733f696
Rudolf Thoeni <rudolf.thoeni@idm-suedtirol.com>
2021-01-26 15:35:49 +0100

    adding eventshort reduced method

--- OdhApiCore/Controllers/other/CompatiblityApiController.cs
+++ OdhApiCore/Controllers/other/CompatiblityApiController.cs
@@ -875,6 +875,82 @@ namespace OdhApiCore.Controllers.api
 
         //TODO ADD EVENTSHORT REDUCED METHOD
 
+        /// <summary>
+        /// GET EventShort Reduced List
+        /// </summary>
+        /// <param name="language">Localization Language</param>
+        /// <param name="startdate">Format (yyyy-MM-dd HH:mm) default or Unix Timestamp</param>
+        /// <param name="enddate">Format (yyyy-MM-dd HH:mm) default or Unix Timestamp</param>
+        /// <param name="datetimeformat">not provided, use default format, for unix timestamp pass "uxtimestamp"</param>
+        /// <param name="source">Source of the data, (possible values 'Content' or 'EBMS')</param>
+        /// <param name="eventlocation">Event Location, (possible values, 'NOI' or 'EC')</param>
+        /// <param name="onlyactive">'true' if only Events marked as Active by Eurac should be displayed</param>
+        /// <returns>List of EventShortReduced Objects</returns>
+        [ProducesResponseType(typeof(IEnumerable<ActivityPoiReduced>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet, Route("EventShort/Reduced")]
+        public Task<IActionResult> GetReducedAsync(
+            string language = "en", 
+            string? startdate = null, 
+            string? enddate = null, 
+            string? datetimeformat = null, 
+            string? source = null, 
+            string? eventlocation = null,
+            LegacyBool onlyactive = null!, 
+            string? webaddress = null,
+            string? sortorder = "ASC",
+            string? lastchange = null,
+            [ModelBinder(typeof(CommaSeparatedArrayBinder))]
+            string[]? fields = null,
+            string? searchfilter = null,
+            string? rawfilter = null,
+            string? rawsort = null,
+            CancellationToken cancellationToken = default)
+        {
+            return GetEventShortReduced(fields: fields ?? Array.Empty<string>(), language, searchfilter,
+                startdate, enddate, datetimeformat, source, eventlocation, webaddress,
+                onlyactive.Value, sortorder, null, rawfilter, rawsort, cancellationToken);
+        }
+
+
+        private Task<IActionResult> GetEventShortReduced(
+         string[] fields, string? language, string? searchfilter, string? startdate, string? enddate, string? datetimeformat,
+         string? sourcefilter, string? eventlocationfilter, string? webaddressfilter, bool? active, string? sortorder,
+         string? lastchange, string? rawfilter, string? rawsort, CancellationToken cancellationToken)
+        {
+            return DoAsyncReturn(async () =>
+            {
+                string select = $"data#>'\\{{Id\\}}' as \"Id\", data#>>'\\{{EventDescription,{language.ToUpper()}\\}}' as \"Name\"";
+
+                string orderby = $"data#>>'\\{{EventDescription,{language.ToUpper()}\\}}' {sortorder}";
+
+                //Custom Fields filter
+                if (fields.Length > 0)
+                    select += string.Join("", fields.Select(field => $", data#>>'\\{{{field.Replace(".", ",")}\\}}' as \"{field}\""));
+
+
+                EventShortHelper myeventshorthelper = EventShortHelper.Create(startdate, enddate, datetimeformat,
+                    sourcefilter, eventlocationfilter, active, null, webaddressfilter, lastchange, sortorder);
+
+                var query =
+                   QueryFactory.Query()
+                       .SelectRaw("data")
+                       .From("eventeuracnoi")
+                       .EventShortWhereExpression(
+                           idlist: myeventshorthelper.idlist, sourcelist: myeventshorthelper.sourcelist,
+                           eventlocationlist: myeventshorthelper.eventlocationlist, webaddresslist: myeventshorthelper.webaddresslist,
+                           start: myeventshorthelper.start, end: myeventshorthelper.end, activefilter: myeventshorthelper.activefilter,
+                           searchfilter: searchfilter, language: language, lastchange: myeventshorthelper.lastchange,
+                           filterClosedData: FilterClosedData, getbyrooms: false)
+                       .ApplyRawFilter(rawfilter)
+                       .ApplyOrdering(new PGGeoSearchResult() { geosearch = false }, rawsort, orderby);
+
+                // Get whole data
+                return await query.GetAsync<object>();
+            });
+        }
+
 
 
         #endregion
