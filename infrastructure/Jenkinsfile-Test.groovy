26c71fe197802c16b43861e17a31184bcf1a4500
RudiThoeni <30471361+RudiThoeni@users.noreply.github.com>
2021-03-04 18:45:53 +0100

    Update Jenkinsfile-Test.groovy

--- infrastructure/Jenkinsfile-Test.groovy
+++ infrastructure/Jenkinsfile-Test.groovy
@@ -7,7 +7,7 @@ pipeline {
         DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/odh-tourism-api'
         DOCKER_TAG = "test-$BUILD_NUMBER"
 	SERVER_PORT = "1011"        
-        PG_CONNECTION = credentials('odh-tourism-api-test-pg-connection')
+        PG_CONNECTION = credentials('odh-tourism-api-test2-pg-connection')
 	MSS_USER = credentials('odh-tourism-api-test-mss-user')
 	MSS_PSWD = credentials('odh-tourism-api-test-mss-pswd')
 	LCS_USER = credentials('odh-tourism-api-test-lcs-user')
