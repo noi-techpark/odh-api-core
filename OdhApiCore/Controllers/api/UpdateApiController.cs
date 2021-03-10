d43b58fd37cba4b2c7c6e50f33c06a4d1d84ae7c
RudiThoeni <r.thoeni@noi.bz.it>
2021-03-10 11:16:35 +0100

    update api controller added

---
+++ OdhApiCore/Controllers/api/UpdateApiController.cs
@@ -0,0 +1,43 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Helper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using SqlKata.Execution;
+using OdhApiCore.Filters;
+using OdhApiCore.GenericHelpers;
+
+namespace OdhApiCore.Controllers.api
+{
+    [ApiExplorerSettings(IgnoreApi = true)]    
+    [ApiController]
+    public class UpdateApiController : OdhController
+    {        
+        private readonly ISettings settings;
+
+        public UpdateApiController(IWebHostEnvironment env, ISettings settings, ILogger<AlpineBitsController> logger, QueryFactory queryFactory)
+            : base(env, settings, logger, queryFactory)
+        {
+            this.settings = settings;
+        }
+
+        [HttpGet, Route("EBMS/UpdateAll")]
+        public async Task<IActionResult> UpdateAllEBMS(CancellationToken cancellationToken)
+        {
+            //await STARequestHelper.GenerateJSONODHActivityPoiForSTA(QueryFactory, settings.JsonConfig.Jsondir, settings.XmlConfig.Xmldir);
+
+            return Ok("EBMS Eventshorts updated");
+        }
+     
+
+    }
+}
