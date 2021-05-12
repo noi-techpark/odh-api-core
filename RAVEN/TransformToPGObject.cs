b701185bf7e01f4f5f6c47637b17f04e68a1865e
Rudi Thoeni <rudi.thoeni@hotmail.com>
2021-05-12 17:42:20 +0200

    adding odhactivitypoi transformer

--- RAVEN/TransformToPGObject.cs
+++ RAVEN/TransformToPGObject.cs
@@ -31,7 +31,77 @@ namespace RAVEN
                 detail.Value.Longdesc = longdesc;
             }
 
+            data._Meta = GetMetadata(data.Id, "accommodation", "lts", data.LastChange);
+
             return data;
+        }
+
+        public static SmgPoiLinked GetODHActivityPoiPGObject(SmgPoiLinked data)
+        {
+            data.Id = data.Id.ToLower();
+
+            if (data.SyncSourceInterface != null)
+                data.SyncSourceInterface = data.SyncSourceInterface.ToLower();
+
+            if (data.SmgTags != null && data.SmgTags.Count > 0)
+                data.SmgTags = data.SmgTags.Select(x => x.ToLower()).ToList();
+            if (!String.IsNullOrEmpty(data.CustomId))
+                data.CustomId = data.CustomId.ToUpper();
+
+            //Related Content
+            if (data.RelatedContent != null)
+            {
+                List<RelatedContent> relcontentlist = new List<RelatedContent>();
+                foreach (var relatedcontent in data.RelatedContent)
+                {
+                    RelatedContent relatedcontenttotransform = relatedcontent;
+
+                    if (relatedcontent.Type == "acco" || relatedcontent.Type == "event")
+                    {
+                        relatedcontenttotransform.Id = relatedcontenttotransform.Id.ToUpper();
+                    }
+                    else
+                    {
+                        relatedcontenttotransform.Id = relatedcontenttotransform.Id.ToLower();
+                    }
+
+                    relcontentlist.Add(relatedcontenttotransform);
+                }
+
+                data.RelatedContent = relcontentlist;
+            }
+
+            if (data.GpsInfo != null)
+            {
+                int i = 2;
+                foreach (var gpsinfo in data.GpsInfo)
+                {
+                    if (!data.GpsPoints.ContainsKey(gpsinfo.Gpstype))
+                    {
+                        data.GpsPoints.Add(gpsinfo.Gpstype, gpsinfo);
+                    }
+                    else
+                    {
+                        data.GpsPoints.Add("position" + i, gpsinfo);
+                        i++;
+                    }                                            
+                }
+            }            
+
+            string sourcemeta = data.Source.ToLower();
+
+            if (sourcemeta == "common" || sourcemeta == "magnolia" || sourcemeta == "content")
+                sourcemeta = "idm";
+
+            data._Meta = GetMetadata(data.Id, "odhactivitypoi", sourcemeta, data.LastChange);
+
+
+            return data;
+        }
+
+        public static Metadata GetMetadata(string id, string type, string source, Nullable<DateTime> lastupdated = null)
+        {
+            return new Metadata() { Id = id, Type = type, LastUpdate = lastupdated, Source = source };
         }
     }
 }
