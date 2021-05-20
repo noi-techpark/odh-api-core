1128624ff491df2ff539bc7199adda01f6f7ad1c
Rudi Thoeni <rudi.thoeni@hotmail.com>
2021-05-20 14:47:35 +0200

    adding csv parsing prototype

--- STA/GetDataFromSTA.cs
+++ STA/GetDataFromSTA.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Globalization;
+using System.IO;
 
 namespace STA
 {
     public class GetDataFromSTA
     {
-        public const string csvurl = "";
+        public const string csvurl = "c:\\temp\\210208.sudtirolmobil.Verkaufsstellen.csv";
 
         public static void ImportCSVFromSTA()
         {
+            //CSVReader Config
+            //var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            //{
+            //    NewLine = Environment.NewLine,
+            //};
 
+            using (var reader = new StreamReader(csvurl))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<STAVendingPoint>();
+            }
         }
 
     }
--- STA/Models/STAVendingPoint.cs
+++ STA/Models/STAVendingPoint.cs
@@ -8,5 +8,63 @@ namespace STA
 {
     public class STAVendingPoint
     {
+        public string Website { get; set; }
+        public string STA_ID { get; set; }
+        public string Salepoint_Name_STA_DE { get; set; }
+        public string Salepoint_Name_STA_IT { get; set; }
+        public string Salepoint_Name_STA_LAD { get; set; }
+        public string Name_SalePoint_Gasser { get; set; }
+        public string Fakturierungsgruppe { get; set; }
+        public string Codice_cliente { get; set; }
+        public string Verkaufspunkttypus { get; set; }
+        public string Rep_Verkaufsmandat { get; set; }
+        public string Rep_Privacy { get; set; }
+        public string Rappresentate_legale { get; set; }
+        public string Sede_legale { get; set; }
+        public string CAP_sede_legale { get; set; }
+        public string Stadt_sede_legale { get; set; }
+        public string E_Mail { get; set; }
+        public string Pec { get; set; }
+        public string Tel { get; set; }
+        public string SEPA { get; set; }
+        public string IBAN { get; set; }
+        public string Parita_IVA { get; set; }
+        public string VerkaufstellenID_Gasser { get; set; }
+        public string Adresse_DE { get; set; }
+        public string Adresse_IT_EN_LAD { get; set; }
+        public string CAP { get; set; }
+        public string Stadt { get; set; }
+        public string cittaIT_EN_LAD { get; set; }
+        public string CODICE_ISTAT { get; set; }
+        public string Wochentags_Beginn { get; set; }
+        public string Wochentags_Ende { get; set; }
+        public string Pause_Start { get; set; }
+        public string Pause_Ende { get; set; }
+        public string Samstag_Beginn { get; set; }
+        public string Samstag_Ende { get; set; }
+        public string Pause_Samstag_Beginn { get; set; }
+        public string Pause_Samstag_Ende { get; set; }
+        public string Sonntag_Beginn { get; set; }
+        public string Sonntag_Ende { get; set; }
+        public string Pause_Sonntag_Beginn { get; set; }
+        public string Pause_Sonntag_Ende { get; set; }
+        public string LEER { get; set; }
+        public string Zusatzinfo_DE { get; set; }
+        public string Zusatzinfo_IT { get; set; }
+        public string Zusatzinfo_LAD { get; set; }
+        public string Zusatzinfo_EN { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string SuedtirolPassDienste { get; set; }
+        public string SuedtirolPass65PlusBeantragung { get; set; }
+        public string Duplikat { get; set; }
+        public string Wertkarte { get; set; }
+        public string StadtfahrkarteoCitybus { get; set; }
+        public string Mobilcard { get; set; }
+        public string bikemobilCard { get; set; }
+        public string MuseumobilCard { get; set; }
+        public string Kartenkreislaufmobilcard { get; set; }
+        public string Kartenkreislaufmuseummobil { get; set; }
+        public string Kartenkreislaufbikemobil { get; set; }
     }
 }
