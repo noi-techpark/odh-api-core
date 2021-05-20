0b54bdfea20f4bcef7918163a693c3b07e3796f1
Rudi Thoeni <rudi.thoeni@hotmail.com>
2021-05-20 17:41:19 +0200

    redirect only if host is tourism.opendatahub.bz.it to databrowser otherwise to swagger

--- STA/Parser/ParseSTAPois.cs
+++ STA/Parser/ParseSTAPois.cs
@@ -11,6 +11,23 @@ namespace STA
     {
         public static SmgPoiLinked ParseSTAVendingPointToODHActivityPoi(STAVendingPoint vendingpoint)
         {
+            //Integrate this data
+            //Website
+            //STA_ID
+            //Salepoint Name DE
+            //Salepoint Name IT
+            //Salepoint Name EN
+            //Salepoint Name LAD(im Moment noch gleich mit IT)
+            //Adresse DE
+            //Adresse IT/ EN / LAD
+            //CAP
+            //Stadt
+            //Città
+            //Öffnungszeiten(von AD bis AT)
+            //Lat
+            //Long
+            //Angebot von Leistungen(von AW bis BD)
+
             return new SmgPoiLinked();
         }
     }
