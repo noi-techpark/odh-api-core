d0f1b165ed77f2f983fb2d12acd05095f61c4e65
Rudi Thoeni <rudi.thoeni@hotmail.com>
2021-05-27 17:42:30 +0200

    addign some nullchecks

--- OdhApiCore/Controllers/api/STAController.cs
+++ OdhApiCore/Controllers/api/STAController.cs
@@ -188,15 +188,22 @@ namespace OdhApiCore.Controllers.api
                     //LicenseInfo                                                                                                                                    //License
                     odhactivitypoi.LicenseInfo = LicenseHelper.GetLicenseforOdhActivityPoi(odhactivitypoi);
 
-                    //Get Nearest District
-                    var geosearchresult = Helper.GeoSearchHelper.GetPGGeoSearchResult(odhactivitypoi.GpsPoints["position"].Latitude, odhactivitypoi.GpsPoints["position"].Longitude, 10000);
-                    var nearestdistrict = await GetLocationInfo.GetNearestDistrict(QueryFactory, geosearchresult, 1);
+                    if(odhactivitypoi.GpsPoints.ContainsKey("position"))
+                    {
+                        //Get Nearest District
+                        var geosearchresult = Helper.GeoSearchHelper.GetPGGeoSearchResult(odhactivitypoi.GpsPoints["position"].Latitude, odhactivitypoi.GpsPoints["position"].Longitude, 10000);
+                        var nearestdistrict = await GetLocationInfo.GetNearestDistrict(QueryFactory, geosearchresult, 1);
 
-                    //Get LocationInfo Object
-                    var locationinfo = await GetLocationInfo.GetTheLocationInfoDistrict(QueryFactory, nearestdistrict.FirstOrDefault().Id);
+                        if (nearestdistrict != null)
+                        {
+                            //Get LocationInfo Object
+                            var locationinfo = await GetLocationInfo.GetTheLocationInfoDistrict(QueryFactory, nearestdistrict.FirstOrDefault().Id);
 
-                    if (locationinfo != null)
-                        odhactivitypoi.LocationInfo = locationinfo;
+                            if (locationinfo != null)
+                                odhactivitypoi.LocationInfo = locationinfo;
+                        }
+                    }
+                    
 
                     //Save to PG
                     //Check if data exists
