936a49ff33767db652094dbfa042ebb28644a354
RudiThoeni <r.thoeni@noi.bz.it>
2021-06-08 11:46:02 +0200

    adding license count stuff

--- Helper/PostgresLicenseCountHelper.cs
+++ Helper/PostgresLicenseCountHelper.cs
@@ -25,17 +25,14 @@ namespace Helper
                 .SelectRaw("id")
                 .From(tablename)
                 .WhereRaw("gen_licenseinfo_closeddata IS NULL")
-                .OrWhereRaw("gen_licenseinfo_closeddata = false")
+                .OrWhereRaw("gen_licenseinfo_closeddata = false");
 
             return await query.CountAsync<long>();
         }
 
-        public static async Task<long> GetAllLicenses(QueryFactory QueryFactory, string tablename)
+        public static async Task<long> GetTotalCountCC0(QueryFactory QueryFactory, string tablename)
         {
-            List<PGParameters> parameters = new List<PGParameters>();
-
-            string whereexpression = "data @> '{\"ImageGallery\": [{ \"License\": \"CC0\" }] }'";
-            //parameters.Add(new PGParameters() { Name = "gallerylicense", Type = NpgsqlTypes.NpgsqlDbType.Jsonb, Value = "{\"ImageGallery\": [{ \"License\": \"CC0\" }] }" });
+            string whereexpression = "data @> '{\"LicenseInfo\": { \"License\": \"CC0\" } }'";
 
             var query = QueryFactory.Query()
                 .SelectRaw("id")
@@ -45,13 +42,10 @@ namespace Helper
             return await query.CountAsync<long>();
         }
 
-        public static async Task<long> GetAllLicenses(QueryFactory QueryFactory, string tablename)
+        public static async Task<long> GetAllDataWithCC0Image(QueryFactory QueryFactory, string tablename)
         {
-            List<PGParameters> parameters = new List<PGParameters>();
-
             string whereexpression = "data @> '{\"ImageGallery\": [{ \"License\": \"CC0\" }] }'";
-            //parameters.Add(new PGParameters() { Name = "gallerylicense", Type = NpgsqlTypes.NpgsqlDbType.Jsonb, Value = "{\"ImageGallery\": [{ \"License\": \"CC0\" }] }" });
-
+          
             var query = QueryFactory.Query()
                 .SelectRaw("id")
                 .From(tablename)
@@ -60,7 +54,7 @@ namespace Helper
             return await query.CountAsync<long>();
         }
 
-        public static async Task<long> GetAllImagesWithLicenseCount(QueryFactory QueryFactory, string tablename)
+        public static async Task<long> GetAllImagesWithCC0License(QueryFactory QueryFactory, string tablename)
         {
             string selectexpression = "Count(*) from (select jsonb_array_elements(data -> 'ImageGallery') as result1";
             string whereexpression = "data @> '{ \"ImageGallery\" : [ { \"License\" : \"CC0\" } ] }') as result1 where result1 ->> 'License' like 'CC0'";
--- OdhApiCore/Controllers/other/LicenseCountController.cs
+++ OdhApiCore/Controllers/other/LicenseCountController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SqlKata.Execution;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OdhApiCore.Controllers.other
@@ -20,49 +22,111 @@ namespace OdhApiCore.Controllers.other
         {
         }
 
+        /// <summary>
+        /// GET LicenseCount
+        /// </summary>
+        /// <returns>LicenseCountResult Object</returns>                
+        /// <response code="200">List created</response>
+        /// <response code="400">Request Error</response>
+        /// <response code="500">Internal Server Error</response>
+        [ProducesResponseType(typeof(LicenseCountResult), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet, Route("LicenseCount")]
+        public async Task<IActionResult> GetLicenseCount(
+            CancellationToken cancellationToken = default)
+        {
+            return await GetAllLicenseCount(cancellationToken);
+        }
 
+        private async Task<IActionResult> GetAllLicenseCount(CancellationToken cancellationToken)
+        {
+            LicenseCountResult result = new LicenseCountResult();
+
+            result.AccoTotaldataopen = await Helper.PostgresLicenseCountHelper.GetTotalCountOpendata(QueryFactory, "accommodations");
+            result.GastroTotaldataopen = await Helper.PostgresLicenseCountHelper.GetTotalCountOpendata(QueryFactory, "gastronomies");
+            result.EventTotaldataopen = await Helper.PostgresLicenseCountHelper.GetTotalCountOpendata(QueryFactory, "events");
+            result.PoiTotaldataopen = await Helper.PostgresLicenseCountHelper.GetTotalCountOpendata(QueryFactory, "pois");
+            result.ActivityTotaldataopen = await Helper.PostgresLicenseCountHelper.GetTotalCountOpendata(QueryFactory, "activities");
+            result.RegionTotaldataopen = await Helper.PostgresLicenseCountHelper.GetTotalCountOpendata(QueryFactory, "regions");
+            result.TVTotaldataopen = await Helper.PostgresLicenseCountHelper.GetTotalCountOpendata(QueryFactory, "tvs");
+            result.SkiAreaTotaldataopen = await Helper.PostgresLicenseCountHelper.GetTotalCountOpendata(QueryFactory, "skiareas");
+            result.ODHActivityPoiTotaldataopen = await Helper.PostgresLicenseCountHelper.GetTotalCountOpendata(QueryFactory, "smgpois");
+
+            result.AccoTotaldata = await Helper.PostgresLicenseCountHelper.GetTotalCount(QueryFactory, "accommodations");
+            result.GastroTotaldata = await Helper.PostgresLicenseCountHelper.GetTotalCount(QueryFactory, "gastronomies");
+            result.EventTotaldata = await Helper.PostgresLicenseCountHelper.GetTotalCount(QueryFactory, "events");
+            result.PoiTotaldata = await Helper.PostgresLicenseCountHelper.GetTotalCount(QueryFactory, "pois");
+            result.ActivityTotaldata = await Helper.PostgresLicenseCountHelper.GetTotalCount(QueryFactory, "activities");
+            result.RegionTotaldata = await Helper.PostgresLicenseCountHelper.GetTotalCount(QueryFactory, "regions");
+            result.TVTotaldata = await Helper.PostgresLicenseCountHelper.GetTotalCount(QueryFactory, "tvs");
+            result.SkiAreaTotaldata = await Helper.PostgresLicenseCountHelper.GetTotalCount(QueryFactory, "skiareas");
+            result.ODHActivityPoiTotaldata = await Helper.PostgresLicenseCountHelper.GetTotalCount(QueryFactory, "smgpois");
+
+            result.AccoLicenses = await Helper.PostgresLicenseCountHelper.GetAllDataWithCC0Image(QueryFactory, "accommodations");
+            result.GastroLicenses = await Helper.PostgresLicenseCountHelper.GetAllDataWithCC0Image(QueryFactory, "gastronomies");
+            result.EventLicenses = await Helper.PostgresLicenseCountHelper.GetAllDataWithCC0Image(QueryFactory, "events");
+            result.PoiLicenses = await Helper.PostgresLicenseCountHelper.GetAllDataWithCC0Image(QueryFactory, "pois");
+            result.ActivityLicenses = await Helper.PostgresLicenseCountHelper.GetAllDataWithCC0Image(QueryFactory, "activities");
+            result.RegionLicenses = await Helper.PostgresLicenseCountHelper.GetAllDataWithCC0Image(QueryFactory, "regions");
+            result.TVLicenses = await Helper.PostgresLicenseCountHelper.GetAllDataWithCC0Image(QueryFactory, "tvs");
+            result.SkiAreaLicenses = await Helper.PostgresLicenseCountHelper.GetAllDataWithCC0Image(QueryFactory, "skiareas");
+            result.ODHActivityPoiLicenses = await Helper.PostgresLicenseCountHelper.GetAllDataWithCC0Image(QueryFactory, "smgpois");
+
+            result.AccoImagesopenCount = await Helper.PostgresLicenseCountHelper.GetAllImagesWithCC0License(QueryFactory, "accommodations");
+            result.GastroImagesopenCount = await Helper.PostgresLicenseCountHelper.GetAllImagesWithCC0License(QueryFactory, "gastronomies");
+            result.EventImagesopenCount = await Helper.PostgresLicenseCountHelper.GetAllImagesWithCC0License(QueryFactory, "events");
+            result.PoiImagesopenCount = await Helper.PostgresLicenseCountHelper.GetAllImagesWithCC0License(QueryFactory, "pois");
+            result.ActivityImagesopenCount = await Helper.PostgresLicenseCountHelper.GetAllImagesWithCC0License(QueryFactory, "activities");
+            result.RegionImagesopenCount = await Helper.PostgresLicenseCountHelper.GetAllImagesWithCC0License(QueryFactory, "regions");
+            result.TVImagesopenCount = await Helper.PostgresLicenseCountHelper.GetAllImagesWithCC0License(QueryFactory, "tvs");
+            result.SkiAreaImagesopenCount = await Helper.PostgresLicenseCountHelper.GetAllImagesWithCC0License(QueryFactory, "skiareas");
+            result.ODHActivityPoiImagesopenCount = await Helper.PostgresLicenseCountHelper.GetAllImagesWithCC0License(QueryFactory, "smgpois");
+
+            return Ok(result);
+        }
     }
 
     public class LicenseCountResult
     {
-        public int AccoLicenses { get; set; }
-        public int GastroLicenses { get; set; }
-        public int EventLicenses { get; set; }
-        public int PoiLicenses { get; set; }
-        public int ActivityLicenses { get; set; }
-        public int RegionLicenses { get; set; }
-        public int TVLicenses { get; set; }
-        public int SkiAreaLicenses { get; set; }
-        public int ODHActivityPoiLicenses { get; set; }
+        public long AccoLicenses { get; set; }
+        public long GastroLicenses { get; set; }
+        public long EventLicenses { get; set; }
+        public long PoiLicenses { get; set; }
+        public long ActivityLicenses { get; set; }
+        public long RegionLicenses { get; set; }
+        public long TVLicenses { get; set; }
+        public long SkiAreaLicenses { get; set; }
+        public long ODHActivityPoiLicenses { get; set; }
 
-        public int AccoTotaldata { get; set; }
-        public int GastroTotaldata { get; set; }
-        public int EventTotaldata { get; set; }
-        public int PoiTotaldata { get; set; }
-        public int ActivityTotaldata { get; set; }
-        public int RegionTotaldata { get; set; }
-        public int TVTotaldata { get; set; }
-        public int SkiAreaTotaldata { get; set; }
-        public int ODHActivityPoiTotaldata { get; set; }
+        public long AccoTotaldata { get; set; }
+        public long GastroTotaldata { get; set; }
+        public long EventTotaldata { get; set; }
+        public long PoiTotaldata { get; set; }
+        public long ActivityTotaldata { get; set; }
+        public long RegionTotaldata { get; set; }
+        public long TVTotaldata { get; set; }
+        public long SkiAreaTotaldata { get; set; }
+        public long ODHActivityPoiTotaldata { get; set; }
 
-        public int AccoTotaldataopen { get; set; }
-        public int GastroTotaldataopen { get; set; }
-        public int EventTotaldataopen { get; set; }
-        public int PoiTotaldataopen { get; set; }
-        public int ActivityTotaldataopen { get; set; }
-        public int RegionTotaldataopen { get; set; }
-        public int TVTotaldataopen { get; set; }
-        public int SkiAreaTotaldataopen { get; set; }
-        public int ODHActivityPoiTotaldataopen { get; set; }
+        public long AccoTotaldataopen { get; set; }
+        public long GastroTotaldataopen { get; set; }
+        public long EventTotaldataopen { get; set; }
+        public long PoiTotaldataopen { get; set; }
+        public long ActivityTotaldataopen { get; set; }
+        public long RegionTotaldataopen { get; set; }
+        public long TVTotaldataopen { get; set; }
+        public long SkiAreaTotaldataopen { get; set; }
+        public long ODHActivityPoiTotaldataopen { get; set; }
 
-        public int AccoImagesopenCount { get; set; }
-        public int GastroImagesopenCount { get; set; }
-        public int EventImagesopenCount { get; set; }
-        public int PoiImagesopenCount { get; set; }
-        public int ActivityImagesopenCount { get; set; }
-        public int RegionImagesopenCount { get; set; }
-        public int TVImagesopenCount { get; set; }
-        public int SkiAreaImagesopenCount { get; set; }
-        public int ODHActivityPoiImagesopenCount { get; set; }
+        public long AccoImagesopenCount { get; set; }
+        public long GastroImagesopenCount { get; set; }
+        public long EventImagesopenCount { get; set; }
+        public long PoiImagesopenCount { get; set; }
+        public long ActivityImagesopenCount { get; set; }
+        public long RegionImagesopenCount { get; set; }
+        public long TVImagesopenCount { get; set; }
+        public long SkiAreaImagesopenCount { get; set; }
+        public long ODHActivityPoiImagesopenCount { get; set; }
     }
 }
