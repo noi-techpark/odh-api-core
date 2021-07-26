390ba8e6ef75dd1958b21c4d02736cce11719aff
RudiThoeni <r.thoeni@noi.bz.it>
2021-07-26 11:10:32 +0200

    remove unneeded try catch

--- OdhApiImporter/Helpers/EBMS/EBMSImportHelper.cs
+++ OdhApiImporter/Helpers/EBMS/EBMSImportHelper.cs
@@ -25,143 +25,136 @@ namespace OdhApiImporter.Helpers.EBMS
 
         public async Task<string> ImportEbmsEventsToDB()
         {
-            try
+            var resulttuple = ImportEBMSData.GetEbmsEvents(settings.EbmsConfig.User, settings.EbmsConfig.Password);
+            var resulttuplesorted = resulttuple.OrderBy(x => x.Item1.StartDate);
+
+            var currenteventshort = await GetAllEventsShort(DateTime.Now);
+
+            //var result = resulttuple.Select(x => x.Item1).ToList();
+            //var resultsorted = result.OrderBy(x => x.StartDate);
+
+            var updatecounter = 0;
+            var newcounter = 0;
+            var deletecounter = 0;
+
+            foreach (var (eventshort, eventebms) in resulttuplesorted)
             {
-                var resulttuple = ImportEBMSData.GetEbmsEvents(settings.EbmsConfig.User, settings.EbmsConfig.Password);
-                var resulttuplesorted = resulttuple.OrderBy(x => x.Item1.StartDate);
+                bool neweventshort = false;
 
-                var currenteventshort = await GetAllEventsShort(DateTime.Now);
+                var query =
+                   QueryFactory.Query("eventeuracnoi")
+                       .Select("data")
+                       .Where("id", eventshort.Id);
 
-                //var result = resulttuple.Select(x => x.Item1).ToList();
-                //var resultsorted = result.OrderBy(x => x.StartDate);
+                var eventindb = await query.GetFirstOrDefaultAsObject<EventShort>();
 
-                var updatecounter = 0;
-                var newcounter = 0;
-                var deletecounter = 0;
+                //currenteventshort.Where(x => x.EventId == eventshort.EventId).FirstOrDefault();
 
-                foreach (var (eventshort, eventebms) in resulttuplesorted)
+                var changedonDB = DateTime.Now;
+
+                //Fields to not overwrite
+                var imagegallery = new List<ImageGallery>();
+                var eventTextDE = "";
+                var eventTextIT = "";
+                var eventTextEN = "";
+                var videourl = "";
+                Nullable<bool> activeweb = null;
+                List<string> technologyfields = null;
+                List<string> customtagging = null;
+                var webadress = "";
+                List<DocumentPDF> eventdocument = new List<DocumentPDF>();
+                bool? soldout = false;
+                bool? externalorganizer = false;
+
+                if (eventindb != null)
                 {
-                    bool neweventshort = false;
+                    changedonDB = eventindb.ChangedOn;
+                    imagegallery = eventindb.ImageGallery;
+                    eventTextDE = eventindb.EventTextDE;
+                    eventTextIT = eventindb.EventTextIT;
+                    eventTextEN = eventindb.EventTextEN;
+                    activeweb = eventindb.ActiveWeb;
+                    videourl = eventindb.VideoUrl;
+                    technologyfields = eventindb.TechnologyFields;
+                    customtagging = eventindb.CustomTagging;
+                    webadress = eventindb.WebAddress;
+                    externalorganizer = eventindb.ExternalOrganizer;
 
-                    var query =
-                       QueryFactory.Query("eventeuracnoi")
-                           .Select("data")
-                           .Where("id", eventshort.Id);
+                    eventdocument = eventindb.EventDocument;
+                    soldout = eventindb.SoldOut;
+                }
+                else
+                {
+                    neweventshort = true;
+                }
 
-                    var eventindb = await query.GetFirstOrDefaultAsObject<EventShort>();
 
-                    //currenteventshort.Where(x => x.EventId == eventshort.EventId).FirstOrDefault();
+                if (changedonDB != eventshort.ChangedOn)
+                {
 
-                    var changedonDB = DateTime.Now;
+                    eventshort.ImageGallery = imagegallery;
+                    eventshort.EventTextDE = eventTextDE;
+                    eventshort.EventTextIT = eventTextIT;
+                    eventshort.EventTextEN = eventTextEN;
+                    eventshort.ActiveWeb = activeweb;
+                    eventshort.VideoUrl = videourl;
+                    eventshort.TechnologyFields = technologyfields;
+                    eventshort.CustomTagging = customtagging;
+                    if (!String.IsNullOrEmpty(webadress))
+                        eventshort.WebAddress = webadress;
 
-                    //Fields to not overwrite
-                    var imagegallery = new List<ImageGallery>();
-                    var eventTextDE = "";
-                    var eventTextIT = "";
-                    var eventTextEN = "";
-                    var videourl = "";
-                    Nullable<bool> activeweb = null;
-                    List<string> technologyfields = null;
-                    List<string> customtagging = null;
-                    var webadress = "";
-                    List<DocumentPDF> eventdocument = new List<DocumentPDF>();
-                    bool? soldout = false;
-                    bool? externalorganizer = false;
+                    eventshort.SoldOut = soldout;
+                    eventshort.EventDocument = eventdocument;
+                    eventshort.ExternalOrganizer = externalorganizer;
 
-                    if (eventindb != null)
+                    //New If CompanyName is Noi - blablabla assign TechnologyField automatically and Write to Display5 if not empty "NOI"
+                    if (eventshort.CompanyName.StartsWith("NOI - "))
                     {
-                        changedonDB = eventindb.ChangedOn;
-                        imagegallery = eventindb.ImageGallery;
-                        eventTextDE = eventindb.EventTextDE;
-                        eventTextIT = eventindb.EventTextIT;
-                        eventTextEN = eventindb.EventTextEN;
-                        activeweb = eventindb.ActiveWeb;
-                        videourl = eventindb.VideoUrl;
-                        technologyfields = eventindb.TechnologyFields;
-                        customtagging = eventindb.CustomTagging;
-                        webadress = eventindb.WebAddress;
-                        externalorganizer = eventindb.ExternalOrganizer;
+                        if (String.IsNullOrEmpty(eventshort.Display5))
+                            eventshort.Display5 = "NOI";
 
-                        eventdocument = eventindb.EventDocument;
-                        soldout = eventindb.SoldOut;
+                        eventshort.TechnologyFields = AssignTechnologyfieldsautomatically(eventshort.CompanyName, eventshort.TechnologyFields);
+                    }
+
+                    int queryresult = 0;
+
+                    //Setting LicenseInfo
+                    eventshort.LicenseInfo = Helper.LicenseHelper.GetLicenseInfoobject<EventShort>(eventshort, Helper.LicenseHelper.GetLicenseforEventShort);
+
+
+                    if (neweventshort)
+                    {
+                        queryresult = await QueryFactory.Query("eventeuracnoi")
+                            //.InsertAsync(new JsonBData() { id = eventshort.Id.ToLower(), data = new JsonRaw(eventshort) });
+                            .InsertAsync(new JsonBDataRaw() { id = eventshort.Id.ToLower(), data = new JsonRaw(eventshort), raw = JsonConvert.SerializeObject(eventebms) });
+
+                        newcounter++;
                     }
                     else
                     {
-                        neweventshort = true;
+                        //var query = await QueryFactory.Query("alpinebits").UpdateAsync(new JsonBData() { id = id, data = new JsonRaw(input) });
+
+                        //TODO CHECK IF THIS WORKS     
+                        queryresult = await QueryFactory.Query("eventeuracnoi").Where("id", eventshort.Id)
+                            //.UpdateAsync(new JsonBData() { id = eventshort.Id.ToLower(), data = new JsonRaw(eventshort) });
+                            .UpdateAsync(new JsonBDataRaw() { id = eventshort.Id.ToLower(), data = new JsonRaw(eventshort), raw = JsonConvert.SerializeObject(eventebms) });
+
+                        updatecounter++;
                     }
 
+                    //if (queryresult != "1")
+                    //    Constants.tracesource.TraceEvent(TraceEventType.Error, 0, "EventShort save error: " + eventshort.Id);
 
-                    if (changedonDB != eventshort.ChangedOn)
-                    {
-
-                        eventshort.ImageGallery = imagegallery;
-                        eventshort.EventTextDE = eventTextDE;
-                        eventshort.EventTextIT = eventTextIT;
-                        eventshort.EventTextEN = eventTextEN;
-                        eventshort.ActiveWeb = activeweb;
-                        eventshort.VideoUrl = videourl;
-                        eventshort.TechnologyFields = technologyfields;
-                        eventshort.CustomTagging = customtagging;
-                        if (!String.IsNullOrEmpty(webadress))
-                            eventshort.WebAddress = webadress;
-
-                        eventshort.SoldOut = soldout;
-                        eventshort.EventDocument = eventdocument;
-                        eventshort.ExternalOrganizer = externalorganizer;
-
-                        //New If CompanyName is Noi - blablabla assign TechnologyField automatically and Write to Display5 if not empty "NOI"
-                        if (eventshort.CompanyName.StartsWith("NOI - "))
-                        {
-                            if (String.IsNullOrEmpty(eventshort.Display5))
-                                eventshort.Display5 = "NOI";
-
-                            eventshort.TechnologyFields = AssignTechnologyfieldsautomatically(eventshort.CompanyName, eventshort.TechnologyFields);
-                        }
-
-                        int queryresult = 0;
-
-                        //Setting LicenseInfo
-                        eventshort.LicenseInfo = Helper.LicenseHelper.GetLicenseInfoobject<EventShort>(eventshort, Helper.LicenseHelper.GetLicenseforEventShort);
-
-
-                        if (neweventshort)
-                        {
-                            queryresult = await QueryFactory.Query("eventeuracnoi")
-                                //.InsertAsync(new JsonBData() { id = eventshort.Id.ToLower(), data = new JsonRaw(eventshort) });
-                                .InsertAsync(new JsonBDataRaw() { id = eventshort.Id.ToLower(), data = new JsonRaw(eventshort), raw = JsonConvert.SerializeObject(eventebms) });
-
-                            newcounter++;
-                        }
-                        else
-                        {
-                            //var query = await QueryFactory.Query("alpinebits").UpdateAsync(new JsonBData() { id = id, data = new JsonRaw(input) });
-
-                            //TODO CHECK IF THIS WORKS     
-                            queryresult = await QueryFactory.Query("eventeuracnoi").Where("id", eventshort.Id)
-                                //.UpdateAsync(new JsonBData() { id = eventshort.Id.ToLower(), data = new JsonRaw(eventshort) });
-                                .UpdateAsync(new JsonBDataRaw() { id = eventshort.Id.ToLower(), data = new JsonRaw(eventshort), raw = JsonConvert.SerializeObject(eventebms) });
-
-                            updatecounter++;
-                        }
-
-                        //if (queryresult != "1")
-                        //    Constants.tracesource.TraceEvent(TraceEventType.Error, 0, "EventShort save error: " + eventshort.Id);
-
-                        //Console.ForegroundColor = ConsoleColor.Green;
-                        //Console.WriteLine("EventShort imported: " + eventshort.Id);
-                        //Constants.tracesource.TraceEvent(TraceEventType.Information, 0, "EventShort imported: " + eventshort.Id);                        
-                    }
+                    //Console.ForegroundColor = ConsoleColor.Green;
+                    //Console.WriteLine("EventShort imported: " + eventshort.Id);
+                    //Constants.tracesource.TraceEvent(TraceEventType.Information, 0, "EventShort imported: " + eventshort.Id);                        
                 }
-
-                if (resulttuple.Select(x => x.Item1).Count() > 0)
-                    deletecounter = await DeleteDeletedEvents(resulttuple.Select(x => x.Item1).ToList(), currenteventshort.ToList());
-
-                return String.Format("Events Updated {0} New {1} Deleted {2}", updatecounter.ToString(), newcounter.ToString(), deletecounter.ToString());
             }
-            catch (Exception ex)
-            {
-                return ex.Message;
-            }
+
+            if (resulttuple.Select(x => x.Item1).Count() > 0)
+                deletecounter = await DeleteDeletedEvents(resulttuple.Select(x => x.Item1).ToList(), currenteventshort.ToList());
+
+            return String.Format("Events Updated {0} New {1} Deleted {2}", updatecounter.ToString(), newcounter.ToString(), deletecounter.ToString());
         }
 
         private static List<string> AssignTechnologyfieldsautomatically(string companyname, List<string> technologyfields)
