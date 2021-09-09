540f6e7b31a6a6e742b7af68604cf08029dad20a
RudiThoeni <r.thoeni@noi.bz.it>
2021-09-09 17:57:54 +0200

    test setting preauthenticate

--- RAVEN/GetDataFromRaven.cs
+++ RAVEN/GetDataFromRaven.cs
@@ -3,6 +3,7 @@ using System;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -21,11 +22,16 @@ namespace RAVEN
                 CredentialCache wrCache = new CredentialCache();
                 wrCache.Add(new Uri(requesturl), "Basic", new NetworkCredential(username, password));
 
-                using (var handler = new HttpClientHandler { Credentials = wrCache })
+                //If handler is sending the request twice add manually the header client.DefaultRequestHeaders.Add(HttpRequestHeader.Authorization, "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(Context.UserName + ":" + Context.Password)));
+
+                using (var handler = new HttpClientHandler { Credentials = wrCache, PreAuthenticate = true })
                 {
                     using (var client = new HttpClient(handler))
                     {
+                        //client.DefaultRequestHeaders.Referrer = new Uri();
+
                         client.Timeout = TimeSpan.FromSeconds(20);
+                        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic ", Convert.ToBase64String(Encoding.UTF8.GetBytes(username + ":" + password)));
 
                         var myresponse = await client.GetAsync(requesturl);
 
