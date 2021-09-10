524b55de05caadebb8b7e69f18a36f988dc34f56
RudiThoeni <r.thoeni@noi.bz.it>
2021-09-10 09:25:15 +0200

    adding string 2 hash extensions

---
+++ Helper/String2HashExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public static class String2HashExtensions
+    {
+        public static string ComputeSHA256Hash(this string value)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(value));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static string ComputeSHA1Hash(this string value)
+        {
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                var inputBytes = Encoding.ASCII.GetBytes(value);
+                var hash = sha1.ComputeHash(inputBytes);
+
+                var sb = new StringBuilder();
+                for (var i = 0; i < hash.Length; i++)
+                {
+                    sb.Append(hash[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
