316913d38c37712104543b9138a79d35307575a0
RudiThoeni <r.thoeni@noi.bz.it>
2021-09-16 17:54:09 +0200

    wip begin Customdataoperation api

---
+++ OdhApiImporter/Helpers/CUSTOM/CustomDataOperation.cs
@@ -0,0 +1,26 @@
+﻿using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdhApiImporter.Helpers.CUSTOM
+{
+    public class CustomDataOperation
+    {
+        private readonly QueryFactory QueryFactory;
+        private readonly ISettings settings;
+
+        public CustomDataOperation(ISettings settings, QueryFactory queryfactory)
+        {
+            this.QueryFactory = queryfactory;
+            this.settings = settings;
+        }
+
+        public void UpdateAllEventShortstonewDataModel()
+        {
+            //Load all data from PG and resave
+            //var eventshorts = QueryFactory.
+        }
+    }
+}
