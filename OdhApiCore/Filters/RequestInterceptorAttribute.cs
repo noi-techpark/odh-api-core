4ae1c9dac729b9494d8d7005f84b1a6a9a7b0646
RudiThoeni <r.thoeni@noi.bz.it>
2021-09-21 17:32:16 +0200

    wip Request Interceptor

--- OdhApiCore/Filters/RequestInterceptorAttribute.cs
+++ OdhApiCore/Filters/RequestInterceptorAttribute.cs
@@ -24,21 +24,25 @@ namespace OdhApiCore.Filters
         {
             context.ActionDescriptor.RouteValues.TryGetValue("action", out string? actionid);
 
-            if (GetActionsToIntercept(actionid))
+            var actiontointercept = GetActionsToIntercept(actionid);
+
+            if (actiontointercept != null)
             {
                 //Configure here the Actions where Interceptor should do something and configure it globally
 
                 //Getting the Querystrings
-                var actionarguments = context.ActionArguments;
+                GetQueryStringsToIntercept(actiontointercept, context.ActionArguments);
                 //bool? availabilitycheck = ((LegacyBool)actionarguments["availabilitycheck"]).Value;  //EX
 
-                //Getting Referer
-                var httpheaders = context.HttpContext.Request.Headers;
-                var referer = httpheaders["Referer"].ToString();
+                ////Getting Referer
+                //var httpheaders = context.HttpContext.Request.Headers;
+                //var referer = httpheaders["Referer"].ToString();
 
                 //actionarguments["availabilitycheck"]).Value;
 
-                await GetReturnObject(context, actionid ?? "", actionarguments, httpheaders);                
+                //await GetReturnObject(context, actionid ?? "", actionarguments, httpheaders);
+                
+                await base.OnActionExecutionAsync(context, next);
             }
             else
             {
@@ -54,32 +58,39 @@ namespace OdhApiCore.Filters
 
         //}
 
-        public bool GetActionsToIntercept(string? actionid)
+        public RequestInterceptorConfig? GetActionsToIntercept(string? actionid)
         {
             if (settings.RequestInterceptorConfig != null && settings.RequestInterceptorConfig.Where(x => x.Route == actionid).Count() > 0)
-                return true;
+                return settings.RequestInterceptorConfig.Where(x => x.Route == actionid).FirstOrDefault();
             else
-                return false;
+                return null;
         }
 
-        public Task GetReturnObject(ActionExecutingContext context, string action, IDictionary<string, object> actionarguments, IHeaderDictionary headerDictionary)
-        {     
-            var language = (string?)actionarguments["language"];
+        public void GetQueryStringsToIntercept(RequestInterceptorConfig config, IDictionary<string,object> querystrings)
+        {
+            //Forget about cancellationtoken and other generated
 
-            string fileName = Path.Combine(settings.JsonConfig.Jsondir, $"STAAccommodations_{language}.json");
 
-            using (StreamReader r = new StreamReader(fileName))
-            {
-                string json = r.ReadToEndAsync().Result;
-
-                //var response = this.Request.CreateResponse(HttpStatusCode.OK);
-                //response.Content = new StringContent(json, Encoding.UTF8, "application/json");
-                //return ResponseMessage(response);
-
-                context.Result = new OkObjectResult(new JsonRaw(json));
-            }
-
-            return Task.CompletedTask;
         }
+
+        //public Task GetReturnObject(ActionExecutingContext context, string action, IDictionary<string, object> actionarguments, IHeaderDictionary headerDictionary)
+        //{     
+        //    var language = (string?)actionarguments["language"];
+
+        //    string fileName = Path.Combine(settings.JsonConfig.Jsondir, $"STAAccommodations_{language}.json");
+
+        //    using (StreamReader r = new StreamReader(fileName))
+        //    {
+        //        string json = r.ReadToEndAsync().Result;
+
+        //        //var response = this.Request.CreateResponse(HttpStatusCode.OK);
+        //        //response.Content = new StringContent(json, Encoding.UTF8, "application/json");
+        //        //return ResponseMessage(response);
+
+        //        context.Result = new OkObjectResult(new JsonRaw(json));
+        //    }
+
+        //    return Task.CompletedTask;
+        //}
     }
 }
