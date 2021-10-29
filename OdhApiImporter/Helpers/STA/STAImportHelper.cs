480fd6b38fb75f21c85b4ede02de5a4fe53648bb
RudiThoeni <r.thoeni@noi.bz.it>
2021-10-29 11:59:51 +0200

    adding STA Importhelper to WS

---
+++ OdhApiImporter/Helpers/STA/STAImportHelper.cs
@@ -0,0 +1,106 @@
+﻿using DataModel;
+using Helper;
+using Microsoft.AspNetCore.Http;
+using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdhApiImporter.Helpers
+{
+    public class STAImportHelper
+    {
+        private readonly QueryFactory QueryFactory;
+        private readonly ISettings settings;
+
+        public STAImportHelper(ISettings settings, QueryFactory queryfactory)
+        {
+            this.QueryFactory = queryfactory;
+            this.settings = settings;
+        }
+
+        private static async Task<string> ReadStringDataManual(HttpRequest request)
+        {
+            using (StreamReader reader = new StreamReader(request.Body, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+
+        private async Task<UpdateDetail> PostVendingPointsFromSTA(HttpRequest request)
+        {
+            string jsonContent = await ReadStringDataManual(request);
+
+            if (!string.IsNullOrEmpty(jsonContent))
+            {
+                return await ImportVendingPointsFromCSV(jsonContent);
+            }
+            else
+                throw new Exception("no Content");
+        }
+
+        private async Task<UpdateDetail> ImportVendingPointsFromCSV(string csvcontent)
+        {
+            var vendingpoints = await STA.GetDataFromSTA.ImportCSVFromSTA(csvcontent);
+
+            if (vendingpoints.Success)
+            {
+                var updatecounter = 0;
+                var newcounter = 0;
+                var deletecounter = 0;
+
+                //Import Each STA Vendingpoi to ODH
+                foreach (var vendingpoint in vendingpoints.records)
+                {
+                    //Parse to ODHActivityPoi
+                    var odhactivitypoi = STA.ParseSTAPois.ParseSTAVendingPointToODHActivityPoi(vendingpoint);
+
+                    //MetaData
+                    odhactivitypoi._Meta = MetadataHelper.GetMetadataobject<ODHActivityPoiLinked>(odhactivitypoi, MetadataHelper.GetMetadataforOdhActivityPoi); //GetMetadata(data.Id, "odhactivitypoi", sourcemeta, data.LastChange);
+                    //LicenseInfo                                                                                                                                    //License
+                    odhactivitypoi.LicenseInfo = LicenseHelper.GetLicenseforOdhActivityPoi(odhactivitypoi);
+
+                    if (odhactivitypoi.GpsPoints.ContainsKey("position"))
+                    {
+                        //Get Nearest District
+                        var geosearchresult = Helper.GeoSearchHelper.GetPGGeoSearchResult(odhactivitypoi.GpsPoints["position"].Latitude, odhactivitypoi.GpsPoints["position"].Longitude, 10000);
+                        var nearestdistrict = await GetLocationInfo.GetNearestDistrict(QueryFactory, geosearchresult, 1);
+
+                        if (nearestdistrict != null && nearestdistrict.Count() > 0)
+                        {
+                            //Get LocationInfo Object
+                            var locationinfo = await GetLocationInfo.GetTheLocationInfoDistrict(QueryFactory, nearestdistrict.FirstOrDefault()?.Id);
+
+                            if (locationinfo != null)
+                                odhactivitypoi.LocationInfo = locationinfo;
+                        }
+                    }
+
+                    //Adding TypeInfo Additional
+                    odhactivitypoi.AdditionalPoiInfos = await GetAdditionalTypeInfo.GetAdditionalTypeInfoForPoi(QueryFactory, odhactivitypoi?.SubType, new List<string>() { "de", "it", "en" });
+
+                    //Save to PG
+                    //Check if data exists                    
+
+                    var result = await QueryFactory.UpsertData<ODHActivityPoi>(odhactivitypoi!, "smgpois");
+
+                    if (result.updated != null)
+                        updatecounter = updatecounter + result.updated.Value;
+                    if (result.created != null)
+                        newcounter = newcounter + result.created.Value;
+                    if (result.deleted != null)
+                        deletecounter = deletecounter + result.deleted.Value;
+                }
+
+                return new UpdateDetail() { created = newcounter, updated = updatecounter, deleted = deletecounter };
+            }
+            else if (vendingpoints.Error)
+                throw new Exception(vendingpoints.ErrorMessage);
+            else
+                throw new Exception("no data to import");
+        }
+    }
+}
