586f83d002027b36ea9ce8d19f64ab1918c6d0bf
RudiThoeni <r.thoeni@noi.bz.it>
2022-03-03 14:03:45 +0100

    adding DSSImportHelper skeleton

---
+++ OdhApiImporter/Helpers/DSS/DSSImportHelper.cs
@@ -0,0 +1,30 @@
+﻿using DataModel;
+using SqlKata.Execution;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OdhApiImporter.Helpers.DSS
+{
+    public class DSSImportHelper : IImportHelper
+    {
+        private readonly QueryFactory QueryFactory;
+        private readonly ISettings settings;
+
+        public DSSImportHelper(ISettings settings, QueryFactory queryfactory)
+        {
+            this.QueryFactory = queryfactory;
+            this.settings = settings;
+        }
+
+        public Task<Tuple<int, int>> DeleteOrDisableData(string id, bool delete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UpdateDetail> SaveDataToODH(DateTime? lastchanged = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
