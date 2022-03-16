1e64147c8999f3cc4c7f197ae9d68f3782249c58
RudiThoeni <r.thoeni@noi.bz.it>
2022-03-16 11:22:37 +0100

    wip parsing dss data

--- DSS/Parser/ParseDSSToODHActivityPoi.cs
+++ DSS/Parser/ParseDSSToODHActivityPoi.cs
@@ -8,12 +8,42 @@ using System.Threading.Tasks;
 namespace DSS.Parser
 {
     public class ParseDSSToODHActivityPoi
-    {
-        public ODHActivityPoiLinked ParseDSSDataToODHActivityPoi(dynamic dssinput)
-        {
+    {        
+        public static ODHActivityPoiLinked ParseDSSDataToODHActivityPoi(ODHActivityPoiLinked myodhactivitypoilinked, dynamic dssitem)
+        {            
+            myodhactivitypoilinked.Id = "dss_" + dssitem.rid;
+            myodhactivitypoilinked.CustomId = dssitem.pid;
 
-            return null;
+            myodhactivitypoilinked.HasLanguage = new List<string>() { "de", "it", "en" };
+
+            if (myodhactivitypoilinked.FirstImport == null)
+                myodhactivitypoilinked.FirstImport = DateTime.Now;
+
+            //name
+
+                //description
+
+                //info-text
+
+                //info-text-summer
+
+                //lifttype TODO Mapping
+
+                //skiresort TODO Mapping 
+
+                //Operationschedule (opening-times, opening-times-summer, season-summer, season-winter)
+
+                //Properties (length, capacity, capacity-per-hour, altitude-start, altitude-end, height-difference, summercard-points, bike-transport, duration)
+
+                //Other (regionId, duration, state-winter, state-summer, datacenterId, number, winterOperation, sorter, summerOperation, sorterSummer)
+
+                //Location (location, locationMountain, gePositionFile)
+
+            return myodhactivitypoilinked;
         }
 
     }
 }
+
+
+//liftbase
--- OdhApiImporter/Helpers/DSS/DSSImportHelper.cs
+++ OdhApiImporter/Helpers/DSS/DSSImportHelper.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using DSS;
 using System.Collections.Generic;
+using DSS.Parser;
 
 namespace OdhApiImporter.Helpers.DSS
 {
@@ -36,10 +37,37 @@ namespace OdhApiImporter.Helpers.DSS
                 dssdata.Add(await GetDSSData.GetDSSDataAsync(requesttype, settings.DSSConfig.User, settings.DSSConfig.Password, settings.DSSConfig.ServiceUrl));
             }
 
-            //TODO Parse DSS Data
+            //Parse DSS Data
+            var parsedresuld = await ParseDSSDataToODHActivityPoi(dssdata[0], dssdata[1]);
 
+            //Insert in DB
 
             throw new NotImplementedException();
+        }
+
+        //Parse the dss interface content
+        public async Task<IEnumerable<Tuple<ODHActivityPoiLinked, dynamic>>> ParseDSSDataToODHActivityPoi(dynamic dssinputbase, dynamic dssinputstatus)
+        {
+            List<Tuple<ODHActivityPoiLinked, dynamic>> myparseddatalist = new List<Tuple<ODHActivityPoiLinked, dynamic>>();
+
+            //interface lastupdate
+            var lastupdate = Convert.ToDateTime(dssinputbase.lastUpdate);
+
+            //loop trough items
+            foreach (var item in dssinputbase.items)
+            {
+                //Get the ODH Item
+                var mydssquery = QueryFactory.Query("smgpois")
+                  .Select("data")
+                  .Where("id", "dss_" + item.rid.ToLower());
+
+                var odhactivitypoiindb = await mydssquery.GetFirstOrDefaultAsObject<ODHActivityPoiLinked>();
+
+                var odhactivitypoi = ParseDSSToODHActivityPoi.ParseDSSDataToODHActivityPoi(item, odhactivitypoiindb);
+                myparseddatalist.Add(Tuple.Create(odhactivitypoi, item));
+            }
+
+            return myparseddatalist;
         }
     }
 }
