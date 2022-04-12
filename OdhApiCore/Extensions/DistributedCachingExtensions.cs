faa19a556181f9c8e613ddf73f2314bc34a1a4ed
RudiThoeni <r.thoeni@noi.bz.it>
2022-04-12 10:15:12 +0200

    adding static classes for extension

---
+++ OdhApiCore/Extensions/DistributedCachingExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OdhApiCore
+{
+    public static class DistributedCachingExtensions
+    {
+        public async static Task SetCacheValueAsync<T>(this IDistributedCache distributedCache, string key, T value, CancellationToken token = default) where T : notnull
+        {
+            await distributedCache.SetAsync(key, value.ToByteArray(), token);
+        }
+
+        public async static Task SetCacheValueAsync<T>(this IDistributedCache distributedCache, string key, TimeSpan timewindow, T value, CancellationToken token = default) where T : notnull
+        {
+            var options = new DistributedCacheEntryOptions { AbsoluteExpiration = DateTimeOffset.Now.Add(timewindow) };
+            await distributedCache.SetAsync(key, value.ToByteArray(), options, token);
+        }
+
+        public async static Task<T?> GetCacheValueAsync<T>(this IDistributedCache distributedCache, string key, CancellationToken token = default(CancellationToken)) where T : class
+        {
+            var result = await distributedCache.GetAsync(key, token);
+            return result.FromByteArray<T>();
+        }
+    }
+}
