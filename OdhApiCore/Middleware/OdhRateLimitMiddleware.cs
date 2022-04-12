0cc345a8b8f0dc54a1690fe547a5ce77734ecf84
RudiThoeni <r.thoeni@noi.bz.it>
2022-04-12 11:20:39 +0200

    removing old rate limit lib

--- OdhApiCore/Middleware/OdhRateLimitMiddleware.cs
+++ OdhApiCore/Middleware/OdhRateLimitMiddleware.cs
@@ -1,64 +1,64 @@
-﻿using AspNetCoreRateLimit;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Options;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+﻿//using AspNetCoreRateLimit;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.Extensions.Options;
+//using System.IdentityModel.Tokens.Jwt;
+//using System.Linq;
+//using System.Net.Http.Headers;
+//using System.Threading.Tasks;
 
-namespace OdhApiCore.Middleware
-{
-    class OdhRateLimitConfiguration : RateLimitConfiguration
-    {
-        private readonly IHttpContextAccessor _httpContextAccessor;
+//namespace OdhApiCore.Middleware
+//{
+//    class OdhRateLimitConfiguration : RateLimitConfiguration
+//    {
+//        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public OdhRateLimitConfiguration(
-            IHttpContextAccessor httpContextAccessor,
-            IOptions<IpRateLimitOptions> ipOptions,
-            IOptions<ClientRateLimitOptions> clientOptions)
-            : base(ipOptions, clientOptions)
-        {
-            _httpContextAccessor = httpContextAccessor;
-        }
+//        public OdhRateLimitConfiguration(
+//            IHttpContextAccessor httpContextAccessor,
+//            IOptions<IpRateLimitOptions> ipOptions,
+//            IOptions<ClientRateLimitOptions> clientOptions)
+//            : base(ipOptions, clientOptions)
+//        {
+//            _httpContextAccessor = httpContextAccessor;
+//        }
 
-        public override void RegisterResolvers()
-        {
-            base.RegisterResolvers();
-            //ClientResolvers.Add(new OdhResolveContributor(_httpContextAccessor));
-        }
-    }
+//        public override void RegisterResolvers()
+//        {
+//            base.RegisterResolvers();
+//            //ClientResolvers.Add(new OdhResolveContributor(_httpContextAccessor));
+//        }
+//    }
 
-    class OdhResolveContributor : IClientResolveContributor
-    {
-        private IHttpContextAccessor httpContextAccessor;
+//    class OdhResolveContributor : IClientResolveContributor
+//    {
+//        private IHttpContextAccessor httpContextAccessor;
 
-        public OdhResolveContributor(IHttpContextAccessor httpContextAccessor)
-        {
-            this.httpContextAccessor = httpContextAccessor;
-        }
+//        public OdhResolveContributor(IHttpContextAccessor httpContextAccessor)
+//        {
+//            this.httpContextAccessor = httpContextAccessor;
+//        }
 
-        public Task<string> ResolveClientAsync(HttpContext httpContext)
-        {
-            var request = httpContextAccessor.HttpContext?.Request;
-            var authorization = request!.Headers.Authorization.ToString();
-            if (authorization != "")
-            {
-                //if (AuthenticationHeaderValue.TryParse(authorization, out var auth))
-                //{
-                //    var handler = new JwtSecurityTokenHandler();
-                //    var token = handler.ReadJwtToken(auth.Parameter);
-                //}
-                return Task.FromResult("Authenticated");
-            }
+//        public Task<string> ResolveClientAsync(HttpContext httpContext)
+//        {
+//            var request = httpContextAccessor.HttpContext?.Request;
+//            var authorization = request!.Headers.Authorization.ToString();
+//            if (authorization != "")
+//            {
+//                //if (AuthenticationHeaderValue.TryParse(authorization, out var auth))
+//                //{
+//                //    var handler = new JwtSecurityTokenHandler();
+//                //    var token = handler.ReadJwtToken(auth.Parameter);
+//                //}
+//                return Task.FromResult("Authenticated");
+//            }
 
-            var referer = request!.Headers.Referer.ToString();
+//            var referer = request!.Headers.Referer.ToString();
 
-            if (referer != "")
-            {
-                return Task.FromResult(referer);
-            }
+//            if (referer != "")
+//            {
+//                return Task.FromResult(referer);
+//            }
 
-            return Task.FromResult("Anonymous");
-        }
-    }
-}
+//            return Task.FromResult("Anonymous");
+//        }
+//    }
+//}
