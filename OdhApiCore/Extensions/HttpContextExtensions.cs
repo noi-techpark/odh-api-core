5bffec4dd2217156388066ee35fc9520084390c8
RudiThoeni <r.thoeni@noi.bz.it>
2022-04-12 10:15:12 +0200

    adding static classes for extension

---
+++ OdhApiCore/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OdhApiCore
+{
+    public static class HttpContextExtensions
+    {
+        public async static Task AddRateLimitHeaders(this HttpContext context, int maxrequests, int requestsdone, int timewindow, CancellationToken token = default)
+        {
+            var remainingrequests = maxrequests - requestsdone;
+
+            context.Response.Headers.Add("X-Rate-Limit-Limit", maxrequests.ToString());
+            context.Response.Headers.Add("X-Rate-Limit-Remaining", remainingrequests.ToString());
+            context.Response.Headers.Add("X-Rate-Limit-Reset", timewindow.ToString());
+
+        }
+    }
+}
---
+++ OdhApiCore/Extensions/Serialization.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace OdhApiCore
+{
+    public static class Serialization
+    {
+        public static byte[] ToByteArray<T>(this T objectToSerialize) where T : notnull
+        {
+            return Encoding.Default.GetBytes(JsonConvert.SerializeObject(objectToSerialize));
+        }
+
+        public static T? FromByteArray<T>(this byte[] arrayToDeserialize) where T : class
+        {
+            if (arrayToDeserialize == null)
+            {
+                return default;
+            }
+
+            return JsonConvert.DeserializeObject<T>(Encoding.Default.GetString(arrayToDeserialize));
+        }
+    }
+}
