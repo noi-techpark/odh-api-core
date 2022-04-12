35f4dc70f2871dd604e03232263894b9b9cffe8a
RudiThoeni <r.thoeni@noi.bz.it>
2022-04-12 10:51:45 +0200

    adding middleware

---
+++ OdhApiCore/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace OdhApiCore
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRateLimiting(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RateLimitingMiddleware>();
+        }
+    }
+}
