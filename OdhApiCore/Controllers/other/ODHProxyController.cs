27b93dda067ac7b7186d7949d39f6043b0ed2e1a
Rudi Thoeni <rudi.thoeni@hotmail.com>
2022-04-21 12:42:17 +0200

    addign proxy method

--- OdhApiCore/Controllers/other/ODHProxyController.cs
+++ OdhApiCore/Controllers/other/ODHProxyController.cs
@@ -5,13 +5,24 @@ using System.Threading.Tasks;
 namespace OdhApiCore.Controllers.other
 {
     [ApiExplorerSettings(IgnoreApi = true)]
-    public class ODHProxyController : Controller
+    public class ODHProxyController : ControllerBase
     {
-        [ApiExplorerSettings(IgnoreApi = true)]        
-        [HttpGet, Route("v1/ODHProxy/{url}")]
-        public Task GetProxied(string url)
-        {            
-            return this.HttpProxyAsync(url);
+        
+        [ApiExplorerSettings(IgnoreApi = true)]                
+        [HttpGet, Route("v1/ODHProxy/{*url}")]        
+        public Task GetODHProxy(string url)
+        {
+            var parameter = "?";
+
+            foreach (var paramdict in HttpContext.Request.Query)
+            {
+                parameter = parameter + paramdict.Key + "=" + paramdict.Value;
+            }
+
+
+            var fullurl = url + parameter;
+
+            return this.HttpProxyAsync(fullurl);
         }
     }
 }
