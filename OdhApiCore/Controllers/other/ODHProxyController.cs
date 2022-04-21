cb810482ce4484e2e73133a5e49b566b2a55810b
Rudi Thoeni <rudi.thoeni@hotmail.com>
2022-04-21 11:54:58 +0200

    addign ODH proxy

---
+++ OdhApiCore/Controllers/other/ODHProxyController.cs
@@ -0,0 +1,17 @@
+﻿using AspNetCore.Proxy;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace OdhApiCore.Controllers.other
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ODHProxyController : Controller
+    {
+        [ApiExplorerSettings(IgnoreApi = true)]        
+        [HttpGet, Route("v1/ODHProxy/{url}")]
+        public Task GetProxied(string url)
+        {            
+            return this.HttpProxyAsync(url);
+        }
+    }
+}
