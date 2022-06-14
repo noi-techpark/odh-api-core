cc1483e19461a86d024fce656ec2b8e21f9e14b9
RudiThoeni <r.thoeni@noi.bz.it>
2022-06-14 10:27:52 +0200

    adding FCM datamodels

---
+++ DataModel/FCMModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class FCMModels
+    {
+        public string to { get; set; }
+        public FCMNotification notification { get; set; }
+        public dynamic data { get; set; }
+    }
+
+    public class FCMNotification
+    {
+        public string title { get; set; }
+        public string body { get; set; }
+        public string sound { get; set; }
+        public string link { get; set; }
+        public string icon { get; set; }
+    }
+
+    #region
+}
