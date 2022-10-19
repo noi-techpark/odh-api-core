43c9bad466d4c170544c706c84e406ce59544613
RudiThoeni <r.thoeni@noi.bz.it>
2022-10-19 12:06:55 +0200

    adding get event data cdb

---
+++ OdhApiImporter/Helpers/LTSCDB/LTSCDBEventImportHelper.cs
@@ -0,0 +1,28 @@
+﻿using DataModel;
+using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OdhApiImporter.Helpers.LTSCDB
+{
+    public class LTSCDBEventImportHelper : ImportHelper, IImportHelper
+    {
+        public LTSCDBEventImportHelper(ISettings settings, QueryFactory queryfactory, string table) : base(settings, queryfactory, table)
+        {
+
+        }
+
+        public async Task<UpdateDetail> SaveDataToODH(DateTime? lastchanged = null, List<string>? idlist = null, CancellationToken cancellationToken = default)
+        {
+            //Import the List
+
+            //Import Single Data
+
+            //Deactivate Data
+
+            throw new NotImplementedException();
+        }
+    }
+}
