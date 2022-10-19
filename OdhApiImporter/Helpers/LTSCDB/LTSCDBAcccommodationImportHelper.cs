43b99949ac78a344e88a46089432d59f558c186d
RudiThoeni <r.thoeni@noi.bz.it>
2022-10-19 10:54:29 +0200

    adding Acco import skeleton

---
+++ OdhApiImporter/Helpers/LTSCDB/LTSCDBAcccommodationImportHelper.cs
@@ -0,0 +1,27 @@
+﻿using DataModel;
+using SqlKata.Execution;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OdhApiImporter.Helpers.LTSCDB
+{
+    public class LTSCDBAcccommodationImportHelper : ImportHelper, IImportHelper
+    {
+        public LTSCDBAcccommodationImportHelper(ISettings settings, QueryFactory queryfactory, string table) : base(settings, queryfactory, table)
+        {
+
+        }
+
+        public Task<UpdateDetail> SaveDataToODH(DateTime? lastchanged = null, CancellationToken cancellationToken = default)
+        {
+            //Import the List
+
+            //Import Single Data
+
+            //Deactivate Data
+
+            throw new NotImplementedException();
+        }
+    }
+}
