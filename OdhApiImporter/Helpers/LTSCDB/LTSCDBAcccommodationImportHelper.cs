3dd02a33245f931d74086fdfae56a9e8471b19b8
Rudi Thoeni <rudi.thoeni@hotmail.com>
2022-10-21 10:32:08 +0200

    adding accoimport skeleton

--- OdhApiImporter/Helpers/LTSCDB/LTSCDBAcccommodationImportHelper.cs
+++ OdhApiImporter/Helpers/LTSCDB/LTSCDBAcccommodationImportHelper.cs
@@ -4,6 +4,7 @@ using LCS;
 using SqlKata.Execution;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -20,18 +21,39 @@ namespace OdhApiImporter.Helpers.LTSCDB
         public async Task<UpdateDetail> SaveDataToODH(DateTime? lastchanged = null, List<string>? idlist = null, CancellationToken cancellationToken = default)
         {
             //Import the List
-
+            if(idlist == null)
+                idlist = await ImportList(lastchanged);
 
             //Import Single Data
+            foreach(var data in idlist)
+            {
+
+            }
 
             //Deactivate Data
 
             throw new NotImplementedException();
         }
 
-        private async Task<XDocument> ImportList(DateTime? lastchanged)
+        private async Task<List<string>> ImportList(DateTime? lastchanged)
         {
-            return GetAccommodationDataCDB.GetHotelChangedfromCDB(lastchanged, "1", settings.CDBConfig.Username, settings.CDBConfig.Password, settings.CDBConfig.Url);
+            var importlist = GetAccommodationDataCDB.GetHotelChangedfromCDB(lastchanged, "1", settings.CDBConfig.Username, settings.CDBConfig.Password, settings.CDBConfig.Url);
+
+            var idlist = new List<string>();
+
+            if (lastchanged != null)
+            {
+                idlist = (from x in importlist.Root.Element("Head").Elements("Data")
+                             where Convert.ToDateTime(x.Attribute("A0CoC").Value) >= DateTime.Now.AddDays(-1).Date
+                             select x.Attribute("A0RID").Value).ToList();
+            }
+            else
+            {
+                idlist = (from x in importlist.Root.Element("Head").Elements("Data")
+                             select x.Attribute("A0RID").Value).ToList();
+            }
+
+            return idlist;
         }
     }
 }
