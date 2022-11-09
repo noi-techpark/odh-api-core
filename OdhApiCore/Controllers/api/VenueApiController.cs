6ebbd7dea0093c8e07f0e24160ff169720dfce97
RudiThoeni <r.thoeni@noi.bz.it>
2022-11-09 13:45:16 +0100

    fix venue codes

--- OdhApiCore/Controllers/api/VenueApiController.cs
+++ OdhApiCore/Controllers/api/VenueApiController.cs
@@ -155,7 +155,7 @@ namespace OdhApiCore.Controllers
         /// <response code="400">Request Error</response>
         /// <response code="500">Internal Server Error</response>
         //[OdhCacheOutputUntilToday(23, 59)]
-        [ProducesResponseType(typeof(IEnumerable<VenueType>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IEnumerable<DDVenueCodes>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         //[Authorize(Roles = "DataReader,VenueReader")]
@@ -185,7 +185,7 @@ namespace OdhApiCore.Controllers
         /// <response code="400">Request Error</response>
         /// <response code="500">Internal Server Error</response>
         //[OdhCacheOutputUntilToday(23, 59)]
-        [ProducesResponseType(typeof(VenueType), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(DDVenueCodes), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         //[Authorize(Roles = "DataReader,VenueReader")]
