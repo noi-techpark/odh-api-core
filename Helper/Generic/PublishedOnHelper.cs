77d8bd323080a669a2c6896ab8722af24b2e2dbb
RudiThoeni <r.thoeni@noi.bz.it>
2023-02-03 10:00:02 +0100

    adding publishedonhelper as extensionmethod

--- Helper/Generic/PublishedOnHelper.cs
+++ Helper/Generic/PublishedOnHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,5 +30,71 @@ namespace Helper
 
             return publishedonlist;
         }
+    }
+
+    public static class PublishOnHelperV2
+    {
+        public static List<string> GetPublishenOnList<T>(this T mydata, bool smgactive, ICollection<AllowedTags> allowedtags = null) where T : IIdentifiable, IMetaData, IActivateable, ISource, ISmgTags
+        {
+            Dictionary<string, List<string>> allowedsources = new Dictionary<string, List<string>>()
+            {
+                { "event", new List<string>(){ "lts" } },
+                { "accommodation", new List<string>(){ "lts" } },
+                { "odhactivitypoi", new List<string>(){ "lts","suedtirolwein", "archapp" } }
+            };
+
+
+            List<string> publishedonlist = new List<string>();
+
+            switch (mydata._Meta.Type)
+            {
+                //Accommodations smgactive (Source LTS IDMActive)
+                case "accommodation":
+                    if (smgactive && allowedsources[mydata._Meta.Type].Contains(mydata._Meta.Source))
+                    {
+                        publishedonlist.Add("https://www.suedtirol.info");
+                        publishedonlist.Add("idm-marketplace");
+                    }
+
+                    break;
+                //Event Add all Active Events from now
+                case "event":
+                    if (mydata.Active && allowedsources[mydata._Meta.Type].Contains(mydata._Meta.Source))
+                    {
+                        publishedonlist.Add("https://www.suedtirol.info");
+                        publishedonlist.Add("idm-marketplace");
+                    }
+
+                    break;
+
+                //ODHActivityPoi 
+                case "odhactivitypoi":
+
+                    if (mydata.Active && allowedsources[mydata._Meta.Type].Contains(mydata._Meta.Source))
+                    {
+                        //IF category is whitelisted
+                        if (allowedtags.Select(x => x.Id).ToList().Intersect(mydata.SmgTags).Count() > 0)
+                        {
+
+                            publishedonlist.Add("https://www.suedtirol.info");
+                            publishedonlist.Add("idm-marketplace");
+                        }
+                    }
+
+                    break;
+
+                default:
+                    if (smgactive)
+                    {
+                        publishedonlist.Add("https://www.suedtirol.info");
+                        publishedonlist.Add("idm-marketplace");
+                    }
+
+                    break;
+            }
+
+            return publishedonlist;
+        }
+
     }
 }
