a0fb4f9fc7f4a48d78b4fb746951a1545397e688
Rudi Thoeni <rudi.thoeni@hotmail.com>
2023-07-21 16:19:18 +0200

    adding webcam parser skeletons

---
+++ FERATEL/Parser/ParseFeratelToODH.cs
@@ -0,0 +1,29 @@
+﻿// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using DataModel;
+using Helper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FERATEL
+{
+    public class ParseFeratelToODH
+    {
+        public static WebcamInfoLinked ParseWebcamToWebcamInfo(WebcamInfoLinked webcam, XDocument webcamtoparse)
+        {
+            if (webcam == null)
+                webcam = new WebcamInfoLinked();
+
+            //TODO Parse the Feratel XML
+
+            return webcam;
+        }
+    }
+}
