61b884bb39815b1963f9dd153cb5c214cb9dbf28
RudiThoeni <r.thoeni@noi.bz.it>
2023-08-01 14:46:06 +0200

    adding reuse license header

--- CDB/Parser/ParseEvents.cs
+++ CDB/Parser/ParseEvents.cs
@@ -1,4 +1,8 @@
-﻿using DataModel;
+﻿// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using DataModel;
 using Helper;
 using System;
 using System.Collections.Generic;
