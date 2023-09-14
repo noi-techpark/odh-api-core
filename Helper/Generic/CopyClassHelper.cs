6b2d4be44548d61ded191da7d437262988961f16
Tobias Burger <tobias_burger@hotmail.com>
2023-09-14 15:33:33 +0200

    BinaryFormatter is deprecated.
    The method doesn't seem to be used. If needed it has to be reimplemented. See:  https://learn.microsoft.com/de-de/dotnet/standard/serialization/binaryformatter-security-guide

--- Helper/Generic/CopyClassHelper.cs
+++ Helper/Generic/CopyClassHelper.cs
@@ -49,25 +49,26 @@ namespace Helper
         /// <returns>The copied object.</returns>
         public static T Clone<T>(T source)
         {
-            if (!typeof(T).IsSerializable)
-            {
-                throw new ArgumentException("The type must be serializable.", nameof(source));
-            }
+            throw new NotImplementedException();
+            // if (!typeof(T).IsSerializable)
+            // {
+            //     throw new ArgumentException("The type must be serializable.", nameof(source));
+            // }
 
-            // Don't serialize a null object, simply return the default for that object
-            if (Object.ReferenceEquals(source, null))
-            {
-                return default(T);
-            }
+            // // Don't serialize a null object, simply return the default for that object
+            // if (Object.ReferenceEquals(source, null))
+            // {
+            //     return default(T);
+            // }
 
-            IFormatter formatter = new BinaryFormatter();
-            Stream stream = new MemoryStream();
-            using (stream)
-            {
-                formatter.Serialize(stream, source);
-                stream.Seek(0, SeekOrigin.Begin);
-                return (T)formatter.Deserialize(stream);
-            }
+            // IFormatter formatter = new BinaryFormatter();
+            // Stream stream = new MemoryStream();
+            // using (stream)
+            // {
+            //     formatter.Serialize(stream, source);
+            //     stream.Seek(0, SeekOrigin.Begin);
+            //     return (T)formatter.Deserialize(stream);
+            // }
         }
 
         /// <summary>
