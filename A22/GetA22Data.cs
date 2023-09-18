aefe578508bb2f13ed6cd2bc622f3d3bb32d939c
Rudi Thoeni <rudi.thoeni@hotmail.com>
2023-09-18 12:21:47 +0200

    adding a22 skeleton importer

--- A22/GetA22Data.cs
+++ A22/GetA22Data.cs
@@ -1,4 +1,8 @@
-﻿using Helper.GetData;
+﻿// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Helper.GetData;
 
 namespace A22
 {
---
+++ OdhApiImporter/Helpers/A22/A22ImportHelper.cs
@@ -0,0 +1,30 @@
+﻿// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using DataModel;
+using Helper;
+using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OdhApiImporter.Helpers.A22
+{
+    public class A22ImportHelper : ImportHelper, IImportHelper
+    {
+        //TODO Make BaseUrl configurable in settings
+        public const string serviceurl = @"";
+
+        public A22ImportHelper(ISettings settings, QueryFactory queryfactory, string table, string importerURL) : base(settings, queryfactory, table, importerURL)
+        {
+
+        }
+
+        public Task<UpdateDetail> SaveDataToODH(DateTime? lastchanged = null, List<string>? idlist = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
