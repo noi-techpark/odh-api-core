aae1f701d6b18ed161f7d7cc00b4d46aae5683b0
Rudi Thoeni <rudi.thoeni@hotmail.com>
2023-09-18 12:01:50 +0200

    adding a22 skeleton getdata

---
+++ A22/GetA22Data.cs
@@ -0,0 +1,29 @@
+﻿using Helper.GetData;
+
+namespace A22
+{
+    public class GetA22Data
+    {
+        public static async Task<dynamic?> GetWebcams(string url, string user, string pass)
+        {
+            GetData getdata = new GetData(url, user, pass, null, GetDataAuthenticationOptions.Basic);
+
+            return await getdata.GetDataAsXmlAsync();
+        }
+
+        public static async Task<dynamic?> GetServiceAreas(string url, string user, string pass)
+        {
+            GetData getdata = new GetData(url, user, pass, null, GetDataAuthenticationOptions.Basic);
+
+            return await getdata.GetDataAsXmlAsync();
+        }
+
+        public static async Task<dynamic?> GetTollStations(string url, string user, string pass)
+        {
+            GetData getdata = new GetData(url, user, pass, null, GetDataAuthenticationOptions.Basic);
+
+            return await getdata.GetDataAsXmlAsync();
+        }
+
+    }
+}
