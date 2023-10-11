501271c6777ad6305af174af654fa7ff339d6734
Tobias Burger <tobias_burger@hotmail.com>
2023-10-11 15:19:50 +0200

    Fox compilation error and update documentation

--- OdhApiCore/Controllers/generic/DistinctController.cs
+++ OdhApiCore/Controllers/generic/DistinctController.cs
@@ -43,7 +43,7 @@ namespace OdhApiCore.Controllers
         /// <param name="pagesize">Elements per Page, (default:10)</param>
         /// <param name="seed">Seed '1 - 10' for Random Sorting, '0' generates a Random Seed, 'null' disables Random Sorting, (default:null)</param>
         /// <param name="odhtype">Mandatory search trough Entities (metadata, accommodation, odhactivitypoi, event, webcam, measuringpoint, ltsactivity, ltspoi, ltsgastronomy, article ..... )</param>
-        /// <param name="fields">Mandatory Select a fields for the Distinct Query, More fields are indicated by separator ',' example fields=Source, arrays are selected with a .[*] example HasLanguage.[*] / Features.[*].Id  (default:'null' all fields are displayed). <a href="https://github.com/noi-techpark/odh-docs/wiki/Common-parameters%2C-fields%2C-language%2C-searchfilter%2C-removenullvalues%2C-updatefrom#fields" target="_blank">Wiki fields</a></param>
+        /// <param name="field">Mandatory Select a field for the Distinct Query, example field=Source, arrays are selected with a [*] example HasLanguage[*] / Features[*].Id  (default:'null' all fields are displayed). <a href="https://github.com/noi-techpark/odh-docs/wiki/Common-parameters%2C-fields%2C-language%2C-searchfilter%2C-removenullvalues%2C-updatefrom#fields" target="_blank">Wiki fields</a></param>
         /// <param name="rawfilter"><a href="https://github.com/noi-techpark/odh-docs/wiki/Using-rawfilter-and-rawsort-on-the-Tourism-Api#rawfilter" target="_blank">Wiki rawfilter</a></param>
         /// <param name="rawsort"><a href="https://github.com/noi-techpark/odh-docs/wiki/Using-rawfilter-and-rawsort-on-the-Tourism-Api#rawfilter" target="_blank">Wiki rawsort</a></param>
         /// <param name="getasarray">Get only first selected field as simple string Array</param>        
@@ -69,7 +69,7 @@ namespace OdhApiCore.Controllers
             bool getasarray = false,            
             CancellationToken cancellationToken = default)
         {
-            return await GetDistinct(pagenumber, pagesize, odhtype, fieldstodisplay, seed, rawfilter, rawsort, getasarray, null, cancellationToken);
+            return await GetDistinct(pagenumber, pagesize, odhtype, field, seed, rawfilter, rawsort, getasarray, null, cancellationToken);
         }
 
         #endregion
