a57ff7a494f55cc4dacbc11a6610617fa54311db
Rudi Thoeni <rudi.thoeni@hotmail.com>
2023-10-25 12:07:57 +0200

    adjusting reuse check

--- OdhApiCore/Swagger/SwaggerExtensions.cs
+++ OdhApiCore/Swagger/SwaggerExtensions.cs
@@ -1,4 +1,8 @@
-﻿using Amazon.Runtime.Internal;
+﻿// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Amazon.Runtime.Internal;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
