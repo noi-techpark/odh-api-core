8a8266afd2b8b9af5574079dd68de84ec483ead6
RudiThoeni <r.thoeni@noi.bz.it>
2023-10-30 10:08:43 +0100

    updating pg function

--- Helper/PGDBScripts/Gen_Access_SQL.sql
+++ Helper/PGDBScripts/Gen_Access_SQL.sql
@@ -2,27 +2,25 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-CREATE OR REPLACE FUNCTION public.calculate_access_array(source text, closeddata bool, reduced bool)
-RETURNS text[]
-LANGUAGE plpgsql
-IMMUTABLE
+CREATE OR REPLACE FUNCTION public.calculate_access_array(source text, closeddata boolean, reduced boolean)
+ RETURNS text[]
+ LANGUAGE plpgsql
+ IMMUTABLE
 AS $function$
-begin
--- if data is from source lts and not reduced IDM only access --
-if source = 'lts' and not reduced then return (array['IDM']);
-end if;
--- If data is from source a22 only access A22 --
-if source = 'a22' then return (array['A22']);
-end if;
--- if data is from source LTS and reduced give access to all others --
-if source = 'lts' and reduced and not closeddata then return (array['A22','ANONYMOUS','STA']);
-end if;
--- if data is not from source lts and a22 and not closed data give all access --
-if source <> 'lts' and source <> 'a22' and not closeddata then return (array['A22','ANONYMOUS','IDM','STA']);
-end if;
-return (array['A22','ANONYMOUS','IDM','STA']);
+	begin 
+	if source = null or closeddata = null or reduced = null then return (array['A22','ANONYMOUS','IDM','STA']);
+	end if;
+	if source = 'lts' and not reduced then return (array['IDM']);
+	end if;    
+    if source = 'a22' then return (array['A22']);  
+	end if;
+	if source = 'lts' and reduced and not closeddata then return (array['A22','ANONYMOUS','STA']);
+	end if; 
+	if source <> 'lts' and source <> 'a22' then return (array['A22','ANONYMOUS','IDM','STA']);
+	end if;
+	return (array['A22','ANONYMOUS','IDM','STA']);
 end;
-$function$
+$function$;
 
 --TODO license is not used....
 CREATE OR REPLACE FUNCTION public.calculate_access_array_rawdata(source text, license text)
