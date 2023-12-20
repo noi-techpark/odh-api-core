75e1704722ede4c81aed7e6af21b1e899f4b7ea3
RudiThoeni <r.thoeni@noi.bz.it>
2023-12-20 16:27:19 +0100

    adding sort fixes

--- OdhApiCore/Controllers/api/EventApiController.cs
+++ OdhApiCore/Controllers/api/EventApiController.cs
@@ -51,7 +51,7 @@ namespace OdhApiCore.Controllers
         /// <param name="odhtagfilter">ODH Taglist Filter (refers to Array SmgTags) (String, Separator ',' more Tags possible, available Tags reference to 'v1/ODHTag?validforentity=event'), (default:'null')</param>        
         /// <param name="begindate">BeginDate of Events (Format: yyyy-MM-dd), (default: 'null')</param>
         /// <param name="enddate">EndDate of Events (Format: yyyy-MM-dd), (default: 'null')</param>
-        /// <param name="sort">Sorting Mode of Events ('asc': Ascending simple sort by general begindate, 'desc': simple descent sorting by begindate, 'upcoming': Sort Events by next EventDate matching passed startdate, 'upcomingspecial': Sort Events by next EventDate matching passed startdate, multiple day events are showed at bottom, default: if no sort mode passed, sort by shortname )</param>
+        /// <param name="sort">Sorting Mode of Events ('asc': Ascending simple sort by next begindate, 'desc': simple descent sorting by next begindate, 'upcoming': Sort Events by next EventDate matching passed startdate, 'upcomingspecial': Sort Events by next EventDate matching passed startdate, multiple day events are showed at bottom, default: if no sort mode passed, sort by shortname )</param>
         /// <param name="active">Active Events Filter (possible Values: 'true' only Active Events, 'false' only Disabled Events), (default:'null')</param>
         /// <param name="odhactive">ODH Active (Published) Events Filter (Refers to field OdhActive) Events Filter (possible Values: 'true' only published Events, 'false' only not published Events), (default:'null')</param>                
         /// <param name="source">Filter by Source (Separator ','), (Sources available 'lts','trevilab','drin'),(default: 'null')</param>
@@ -226,25 +226,36 @@ namespace OdhApiCore.Controllers
 
                 if (sort != null)
                 {
-                    //gen_nextbegindate Obsolete
-                    //if (sort.ToLower() == "asc")
-                    //    sortifseednull = "gen_nextbegindate ASC";
-                    //else
-                    //    sortifseednull = "gen_nextbegindate DESC";
-
-                    var sortfromdate = "2000-01-01";
-                    
-                    if(begindate != null)
-                        sortfromdate = begindate;
-                    else if (enddate != null)
-                        sortfromdate = enddate;
-
-                    //TO CHECK Events with Eventdate interval vs singledays, how do we sort here?
-
+                    //simple sort bz datebegin
                     if (sort.ToLower() == "asc")
-                        sortifseednull = "get_nearest_tsrange_distance(gen_eventdatearray, ('" + sortfromdate + "')::timestamp, 'asc'),get_nearest_tsrange(gen_eventdatearray, ('" + sortfromdate + "')::timestamp) DESC";
-                    else
-                        sortifseednull = "get_nearest_tsrange_distance(gen_eventdatearray, ('" + sortfromdate + "')::timestamp, 'desc'),get_nearest_tsrange(gen_eventdatearray, ('" + sortfromdate + "')::timestamp) ASC";
+                        sortifseednull = "gen_nextbegindate ASC";
+                    else if (sort.ToLower() == "desc")
+                        sortifseednull = "gen_nextbegindate DESC";
+                    else if (sort.ToLower().StartsWith("upcoming"))
+                    {
+                        var sortfromdate = "2000-01-01";
+
+                        if (begindate != null)
+                            sortfromdate = begindate;
+                        else if (enddate != null)
+                            sortfromdate = enddate;
+
+                        //TO CHECK Events with Eventdate interval vs singledays, how do we sort here?
+                        if (sort.ToLower() == "upcoming")
+                        {
+                            if (sort.ToLower() == "asc")
+                                sortifseednull = "get_nearest_tsrange_distance(gen_eventdatearray, ('" + sortfromdate + "')::timestamp, 'asc'),get_nearest_tsrange(gen_eventdatearray, ('" + sortfromdate + "')::timestamp) DESC";
+                            else
+                                sortifseednull = "get_nearest_tsrange_distance(gen_eventdatearray, ('" + sortfromdate + "')::timestamp, 'desc') DESC,get_nearest_tsrange(gen_eventdatearray, ('" + sortfromdate + "')::timestamp) ASC";
+                        }
+                        if (sort.ToLower() == "upcomingspecial")
+                        {
+                            if (sort.ToLower() == "asc")
+                                sortifseednull = "get_nearest_tsrange_distance(gen_eventdatearray, ('" + sortfromdate + "')::timestamp, 'asc', true),get_nearest_tsrange(gen_eventdatearray, ('" + sortfromdate + "')::timestamp) DESC";
+                            else
+                                sortifseednull = "get_nearest_tsrange_distance(gen_eventdatearray, ('" + sortfromdate + "')::timestamp, 'desc', true) DESC,get_nearest_tsrange(gen_eventdatearray, ('" + sortfromdate + "')::timestamp) ASC";
+                        }
+                    }
 
                     //Set seed to null
                     seed = null;
