dcd4e0a0073e523ce46d00934e1d54902ae5492c
RudiThoeni <r.thoeni@noi.bz.it>
2024-01-18 09:46:16 +0100

    adding reuse header

--- Helper/Identity/RoleHelper.cs
+++ Helper/Identity/RoleHelper.cs
@@ -1,4 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
