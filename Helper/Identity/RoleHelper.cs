be479d7d5079effe5d224f3932d0273e73548f01
RudiThoeni <r.thoeni@noi.bz.it>
2024-01-18 11:03:29 +0100

    refactor add roles from resource_access

--- Helper/Identity/RoleHelper.cs
+++ Helper/Identity/RoleHelper.cs
@@ -2,11 +2,14 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using Microsoft.AspNetCore.Authorization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,21 +24,30 @@ namespace Helper.Identity
                 var claimsIdentity = principal.Identity as ClaimsIdentity;
                 if (claimsIdentity != null)
                 {
-                    //Get Roles from resource_access 
-                    if (claimsIdentity.Claims.Where(x => x.Type == "resource_access").FirstOrDefault() != null)
+                    JObject obj = JObject.Parse(claimsIdentity.Claims.First(c => c.Type == "resource_access").Value);
+                    var roleAccess = obj.GetValue(client)!.ToObject<JObject>()!.GetValue("roles");
+                    foreach (JToken role in roleAccess!)
                     {
-                        var resourceroles = JsonConvert.DeserializeObject<Dictionary<string, Resource_Roles>>(claimsIdentity.Claims.Where(x => x.Type == "resource_access").FirstOrDefault().Value);
-                        
-                        if(resourceroles != null && resourceroles.Where(x => x.Key == client).Count() > 0)
-                        {
-                            foreach (var resourcerole in resourceroles.Where(x => x.Key == client).FirstOrDefault().Value.roles)
-                            {
-                                //add only if role is not present
-                                if (!principal.IsInRole(resourcerole))
-                                    claimsIdentity.AddClaim(new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", resourcerole));
-                            }
-                        }
+                        //add only if role is not present
+                        if (!principal.IsInRole(role.ToString()))
+                            claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role.ToString()));
                     }
+
+                    ////Get Roles from resource_access 
+                    //if (claimsIdentity.Claims.Where(x => x.Type == "resource_access").FirstOrDefault() != null)
+                    //{
+                    //    var resourceroles = JsonConvert.DeserializeObject<Dictionary<string, Resource_Roles>>(claimsIdentity.Claims.Where(x => x.Type == "resource_access").FirstOrDefault().Value);
+                        
+                    //    if(resourceroles != null && resourceroles.Where(x => x.Key == client).Count() > 0)
+                    //    {
+                    //        foreach (var resourcerole in resourceroles.Where(x => x.Key == client).FirstOrDefault().Value.roles)
+                    //        {
+                    //            //add only if role is not present
+                    //            if (!principal.IsInRole(resourcerole))
+                    //                claimsIdentity.AddClaim(new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", resourcerole));
+                    //        }
+                    //    }
+                    //}
                 }
             }
         }
@@ -47,4 +59,5 @@ namespace Helper.Identity
 
     }
 
+   
 }
