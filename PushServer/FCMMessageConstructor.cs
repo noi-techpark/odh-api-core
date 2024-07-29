0d6c628ca280df0afad578b6bae5069f0d07692a
RudiThoeni <r.thoeni@noi.bz.it>
2024-07-29 13:11:13 +0200

    disabling fcm icon, link and sound property

--- PushServer/FCMMessageConstructor.cs
+++ PushServer/FCMMessageConstructor.cs
@@ -28,10 +28,12 @@ namespace PushServer
                 message.data = new { deep_link = deeplink };
                 FCMNotification notification = new FCMNotification();
 
-                notification.icon = "ic_notification";
-                notification.link = deeplink;
+                //notification.icon = "ic_notification";
+                //notification.link = deeplink;
+
                 notification.title = ((ArticlesLinked)myobject).Detail.ContainsKey(language) && !String.IsNullOrEmpty(((ArticlesLinked)myobject).Detail[language].Title) ? ((ArticlesLinked)myobject).Detail[language].Title : "Noi Community App News";
                 notification.body = ((ArticlesLinked)myobject).Detail.ContainsKey(language) && !String.IsNullOrEmpty(((ArticlesLinked)myobject).Detail[language].AdditionalText) ? ((ArticlesLinked)myobject).Detail[language].AdditionalText : "Check out the latest News on the NOI Community App";
+                
                 //notification.sound = "default";
 
                 message.notification = notification;
@@ -47,10 +49,12 @@ namespace PushServer
                 message.data = new { deep_link = deeplink };
                 FCMNotification notification = new FCMNotification();
 
-                notification.icon = "ic_notification";
-                notification.link = deeplink;
+                //notification.icon = "ic_notification";
+                //notification.link = deeplink;
+
                 notification.title = ((EventShortLinked)myobject).EventTitle.ContainsKey(language) && !String.IsNullOrEmpty(((EventShortLinked)myobject).EventTitle[language]) ? ((EventShortLinked)myobject).EventTitle[language] : "Noi Community App Event";
                 notification.body = ((EventShortLinked)myobject).EventText.ContainsKey(language) && !String.IsNullOrEmpty(((EventShortLinked)myobject).EventText[language]) ? ((EventShortLinked)myobject).EventText[language] : "Check out the latest Events on the NOI Community App";
+                
                 //notification.sound = "default";
 
                 message.notification = notification;
@@ -75,10 +79,12 @@ namespace PushServer
                 messagebody.data = new { deep_link = deeplink };
                 FCMNotification notification = new FCMNotification();
 
-                notification.icon = "ic_notification";
-                notification.link = deeplink;
+                //notification.icon = "ic_notification";
+                //notification.link = deeplink;
+
                 notification.title = ((ArticlesLinked)myobject).Detail.ContainsKey(language) && !String.IsNullOrEmpty(((ArticlesLinked)myobject).Detail[language].Title) ? ((ArticlesLinked)myobject).Detail[language].Title : "Noi Community App News";
                 notification.body = ((ArticlesLinked)myobject).Detail.ContainsKey(language) && !String.IsNullOrEmpty(((ArticlesLinked)myobject).Detail[language].AdditionalText) ? ((ArticlesLinked)myobject).Detail[language].AdditionalText : "Check out the latest News on the NOI Community App";
+                
                 //notification.sound = "default";
 
                 messagebody.notification = notification;
@@ -103,10 +109,12 @@ namespace PushServer
                 if (language.ToLower() == "it")
                     titleprefix = "nuovo evento: ";
 
-                notification.icon = "ic_notification";
-                notification.link = deeplink;
+                //notification.icon = "ic_notification";
+                //notification.link = deeplink;
+
                 notification.title = ((EventShortLinked)myobject).EventTitle.ContainsKey(language) && !String.IsNullOrEmpty(((EventShortLinked)myobject).EventTitle[language]) ? titleprefix + ((EventShortLinked)myobject).EventTitle[language] : "Noi Community App Event";
                 notification.body = ((EventShortLinked)myobject).EventText.ContainsKey(language) && !String.IsNullOrEmpty(((EventShortLinked)myobject).EventText[language]) ? ((EventShortLinked)myobject).EventText[language] : "Check out the latest Events on the NOI Community App";
+                
                 //notification.sound = "default";
 
                 messagebody.notification = notification;
