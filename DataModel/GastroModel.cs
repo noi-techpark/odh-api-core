0ba6387240014db191ed0f836fa9b96adfb2d77f
RudiThoeni <r.thoeni@noi.bz.it>
2024-08-07 13:29:57 +0200

    gastro items test (commented out)

---
+++ DataModel/GastroModel.cs
@@ -0,0 +1,13 @@
+//using System;
+//using DataModel;
+
+//public class GastroItem : IIdentifiable, IActivateable, ILicenseInfo, IShortName, IMetaData, IImportDateassigneable
+//{
+//    public string Id { get; set; }
+//    public bool Active { get; set; }
+//    public LicenseInfo LicenseInfo { get; set; }
+//    public string? Shortname { get; set; }
+//    public Metadata _Meta { get; set; }
+//    public DateTime? FirstImport { get; set; }
+//    public DateTime? LastChange { get; set; }
+//}
