0379b5e224d570f9a03b13df32189f3bdecc1bf8
RudiThoeni <r.thoeni@noi.bz.it>
2024-09-10 11:17:27 +0200

    fix scode sname from echarging spreadsheet

--- NINJA/Parser/ParseNinjaData.cs
+++ NINJA/Parser/ParseNinjaData.cs
@@ -405,6 +405,11 @@ namespace NINJA.Parser
                 {
                     //sname
                     echargingpoi.Detail["en"].Title = data.sname;
+
+                    //If data is from static spreadsheet use scode
+                    if (echargingpoi.Source == "1uccqzavgmvyrpeq-lipffalqawcg4lfpakc2mjt79fy")
+                        echargingpoi.Detail["en"].Title = data.scode;
+
                     echargingpoi.Detail["en"].AdditionalText = data.pname;
                     echargingpoi.Detail.TryAddOrUpdate("en", echargingpoi.Detail["en"]);
                 }
