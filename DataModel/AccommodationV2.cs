0426c41d1d45f35c7608c282fc159a6bf091bec8
RudiThoeni <r.thoeni@noi.bz.it>
2024-10-03 19:21:06 +0200

    usual reuse header fix

--- DataModel/AccommodationV2.cs
+++ DataModel/AccommodationV2.cs
@@ -1,4 +1,8 @@
-﻿using DataModel.Annotations;
+﻿// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using DataModel.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
