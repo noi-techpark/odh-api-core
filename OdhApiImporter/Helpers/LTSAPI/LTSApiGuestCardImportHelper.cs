9e7d0a685c7c81e56def9a468f333a033c96dd5a
RudiThoeni <r.thoeni@noi.bz.it>
2024-10-29 12:23:11 +0100

    updating guestcard import

--- OdhApiImporter/Helpers/LTSAPI/LTSApiGuestCardImportHelper.cs
+++ OdhApiImporter/Helpers/LTSAPI/LTSApiGuestCardImportHelper.cs
@@ -8,9 +8,11 @@ using LTSAPI;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using NINJA.Parser;
+using ServiceReferenceLCS;
 using SqlKata.Execution;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -39,180 +41,201 @@ namespace OdhApiImporter.Helpers.LTSCDB
             {
                 return null;
             }
-        }       
+        }
 
         public async Task<UpdateDetail> SaveDataToODH(DateTime? lastchanged = null, List<string>? idlist = null, CancellationToken cancellationToken = default)
         {
             //Import the List
             var guestcards = await GetGuestcardsFromLTSV2();
             //Import Single Data
-
+            var result = await SaveSuedtirolGuestPassCardTypesToPG(guestcards);
 
             //Deactivate Data
 
             throw new NotImplementedException();
         }
 
-        private async Task<UpdateDetail> SaveSuedtirolGuestPassCardTypesToPG(NinjaObjectWithParent<NinjaEchargingPlug, NinjaEchargingStation> ninjadata)
+        private async Task<UpdateDetail> SaveSuedtirolGuestPassCardTypesToPG(List<JObject> ltsdata)
         {
             var newimportcounter = 0;
             var updateimportcounter = 0;
             var errorimportcounter = 0;
             var deleteimportcounter = 0;
 
-            //List<string> idlistspreadsheet = new List<string>();
-          
-            //var tagquery = QueryFactory.Query("smgtags")
-            //        .Select("data")
-            //        .Where("id", "e-auto ladestation");
-            //var echargingtag = await tagquery.GetObjectSingleAsync<ODHTagLinked>();
+            List<string> idlistlts = new List<string>();
 
-            //foreach (var data in ninjadata.data)
-            //{
-            //    string id = "echarging_" + data.scode.Replace("/", "").ToLower();
+            List<LTSGuestcard> guestcardata = new List<LTSGuestcard>();
 
-            //    //See if data exists
-            //    var query = QueryFactory.Query("smgpois")
-            //        .Select("data")
-            //        .Where("id", id);
+            foreach (var ltsdatasingle in ltsdata)
+            {
+                guestcardata.AddRange(ltsdatasingle["data"].ToObject<IList<LTSGuestcard>>());
+            }
+            
+            foreach (var data in guestcardata)
+            {
+                string id = data.rid;
 
-            //    var objecttosave = await query.GetObjectSingleAsync<ODHActivityPoiLinked>();
+                //See if data exists
+                var query = QueryFactory.Query("tags")
+                    .Select("data")
+                    .Where("id", id);
 
-            //    objecttosave = ParseNinjaData.ParseNinjaEchargingToODHActivityPoi(id, data, objecttosave, echargingtag);
+                var objecttosave = await query.GetObjectSingleAsync<TagLinked>();
 
-            //    if (objecttosave != null)
-            //    {
-            //        //Setting Location Info                    
-            //        if (objecttosave.GpsInfo != null)
-            //        {
-            //            await SetLocationInfo(objecttosave);
-            //        }
+                if (objecttosave == null)
+                    objecttosave = new TagLinked();
 
-            //        objecttosave.Active = true;
-            //        //objecttosave.SmgActive = true;
+                objecttosave.Id = data.rid;
+                objecttosave.Active = data.isActive;
+                objecttosave.DisplayAsCategory = false;
+                objecttosave.FirstImport = objecttosave.FirstImport == null ? DateTime.Now : objecttosave.FirstImport;
+                objecttosave.LastChange = data.lastUpdate;
+                
+                objecttosave.Source = "lts";
+                objecttosave.TagName = data.name;
+                objecttosave.MainEntity = "accommodation";
+                objecttosave.ValidForEntity = new List<string>() { "accommodation" };
+                objecttosave.Shortname = objecttosave.TagName.ContainsKey("en") ? objecttosave.TagName["en"] : objecttosave.TagName.FirstOrDefault().Value;
+                objecttosave.Types = new List<string>() { "cardtype" };
 
-            //        //var idtocheck = kvp.Key;
+                objecttosave.IDMCategoryMapping = null;
+                objecttosave.PublishDataWithTagOn = null;
+                objecttosave.Mapping = null;
+                objecttosave.LTSTaggingInfo = null;
+                objecttosave.PublishedOn = null;
+                objecttosave.MappedTagIds = null;
 
-            //        //if (idtocheck.Length > 50)
-            //        //    idtocheck = idtocheck.Substring(0, 50);
+             
+                var result = await InsertDataToDB(objecttosave, data);
 
-            //        var result = await InsertDataToDB(objecttosave, new KeyValuePair<string, NinjaDataWithParent<NinjaEchargingPlug, NinjaEchargingStation>>(id, data));
+                newimportcounter = newimportcounter + result.created ?? 0;
+                updateimportcounter = updateimportcounter + result.updated ?? 0;
+                errorimportcounter = errorimportcounter + result.error ?? 0;
 
-            //        newimportcounter = newimportcounter + result.created ?? 0;
-            //        updateimportcounter = updateimportcounter + result.updated ?? 0;
-            //        errorimportcounter = errorimportcounter + result.error ?? 0;
+                idlistlts.Add(id);
 
-            //        idlistspreadsheet.Add(id);
+                WriteLog.LogToConsole(id, "dataimport", "single.suedtirolguestpass.cardtypes", new ImportLog() { sourceid = id, sourceinterface = "lts.suedtirolguestpass.cardtypes", success = true, error = "" });
+            }
 
-            //        //if (!sourcelist.Contains(objecttosave.Source))
-            //        //    sourcelist.Add(objecttosave.Source);
+            //Begin SetDataNotinListToInactive
+            var idlistdb = await GetAllDataBySourceAndType(new List<string>() { "lts" }, new List<string>() { "cardtype" });
 
-            //        WriteLog.LogToConsole(id, "dataimport", "single.echarging", new ImportLog() { sourceid = id, sourceinterface = "mobility.echarging", success = true, error = "" });
-            //    }
-            //    else
-            //    {
-            //        WriteLog.LogToConsole(id, "dataimport", "single.echarging", new ImportLog() { sourceid = id, sourceinterface = "mobility.echarging", success = false, error = "echarging could not be parsed" });
-            //    }
-            //}
+            var idstodelete = idlistdb.Where(p => !idlistlts.Any(p2 => p2 == p));
 
-            ////Begin SetDataNotinListToInactive
-            //var idlistdb = await GetAllDataBySource(sourcelist.Select(x => x.Item1).Distinct().ToList());
+            foreach (var idtodelete in idstodelete)
+            {
+                var deletedisableresult = await DeleteOrDisableData(idtodelete, false);
 
-            //var idstodelete = idlistdb.Where(p => !idlistspreadsheet.Any(p2 => p2 == p));
-
-            //foreach (var idtodelete in idstodelete)
-            //{
-            //    var deletedisableresult = await DeleteOrDisableData(idtodelete, false);
-
-            //    if (deletedisableresult.Item1 > 0)
-            //        WriteLog.LogToConsole(idtodelete, "dataimport", "single.echarging.deactivate", new ImportLog() { sourceid = idtodelete, sourceinterface = "mobility.echarging", success = true, error = "" });
-            //    else if (deletedisableresult.Item2 > 0)
-            //        WriteLog.LogToConsole(idtodelete, "dataimport", "single.echarging.delete", new ImportLog() { sourceid = idtodelete, sourceinterface = "mobility.echarging", success = true, error = "" });
+                if (deletedisableresult.Item1 > 0)
+                    WriteLog.LogToConsole(idtodelete, "dataimport", "single.suedtirolguestpass.cardtypes.deactivate", new ImportLog() { sourceid = idtodelete, sourceinterface = "lts.suedtirolguestpass.cardtypes", success = true, error = "" });
+                else if (deletedisableresult.Item2 > 0)
+                    WriteLog.LogToConsole(idtodelete, "dataimport", "single.suedtirolguestpass.cardtypes.delete", new ImportLog() { sourceid = idtodelete, sourceinterface = "lts.suedtirolguestpass.cardtypes", success = true, error = "" });
 
 
-            //    deleteimportcounter = deleteimportcounter + deletedisableresult.Item1 + deletedisableresult.Item2;
-            //}
+                deleteimportcounter = deleteimportcounter + deletedisableresult.Item1 + deletedisableresult.Item2;
+            }
 
             return new UpdateDetail() { updated = updateimportcounter, created = newimportcounter, deleted = deleteimportcounter, error = errorimportcounter };
         }
 
-        //private async Task<PGCRUDResult> InsertDataToDB(ODHActivityPoiLinked objecttosave, KeyValuePair<string, NinjaDataWithParent<NinjaEchargingPlug, NinjaEchargingStation>> ninjadata)
-        //{
-        //    try
-        //    {
-        //        objecttosave.Id = objecttosave.Id?.ToLower();
+        private async Task<PGCRUDResult> InsertDataToDB(TagLinked objecttosave, LTSGuestcard guestcard)
+        {
+            try
+            {
+                objecttosave.Id = objecttosave.Id?.ToLower();
 
-        //        //Set LicenseInfo
-        //        objecttosave.LicenseInfo = Helper.LicenseHelper.GetLicenseInfoobject(objecttosave, Helper.LicenseHelper.GetLicenseforOdhActivityPoi);
+                //Set LicenseInfo
+                objecttosave.LicenseInfo = Helper.LicenseHelper.GetLicenseInfoobject(objecttosave, Helper.LicenseHelper.GetLicenseforTag);
 
-        //        //Setting MetaInfo (we need the MetaData Object in the PublishedOnList Creator)
-        //        objecttosave._Meta = MetadataHelper.GetMetadataobject(objecttosave);
+                //Setting MetaInfo (we need the MetaData Object in the PublishedOnList Creator)
+                objecttosave._Meta = MetadataHelper.GetMetadataobject(objecttosave);
 
-        //        //Set PublishedOn
-        //        objecttosave.CreatePublishedOnList();
+                //Set PublishedOn
+                objecttosave.CreatePublishedOnList();
 
-        //        var rawdataid = await InsertInRawDataDB(ninjadata);
+                var rawdataid = await InsertInRawDataDB(guestcard);
 
-        //        return await QueryFactory.UpsertData<ODHActivityPoiLinked>(objecttosave, "tags", rawdataid, "lts.suedtirolguestpass.import", importerURL);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw new Exception(ex.Message);
-        //    }
-        //}
+                return await QueryFactory.UpsertData<TagLinked>(objecttosave, "tags", rawdataid, "lts.suedtirolguestpass.import", importerURL);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
 
-        //private async Task<int> InsertInRawDataDB(KeyValuePair<string, NinjaDataWithParent<NinjaEchargingPlug, NinjaEchargingStation>> ninjadata)
-        //{
-        //    return await QueryFactory.InsertInRawtableAndGetIdAsync(
-        //                new RawDataStore()
-        //                {
-        //                    datasource = ninjadata.Value.porigin,
-        //                    importdate = DateTime.Now,
-        //                    raw = JsonConvert.SerializeObject(ninjadata.Value),
-        //                    sourceinterface = "echarging",
-        //                    sourceid = ninjadata.Key,
-        //                    sourceurl = "https://mobility.api.opendatahub.com/v2/flat/EChargingPlug/",
-        //                    type = "echarging",
-        //                    license = "open",
-        //                    rawformat = "json"
-        //                });
-        //}
+        private async Task<int> InsertInRawDataDB(LTSGuestcard guestcard)
+        {
+            return await QueryFactory.InsertInRawtableAndGetIdAsync(
+                        new RawDataStore()
+                        {
+                            datasource = "lts",
+                            importdate = DateTime.Now,
+                            raw = JsonConvert.SerializeObject(guestcard),
+                            sourceinterface = "suedtirolguestpass",
+                            sourceid = guestcard.rid,
+                            sourceurl = "https://go.lts.it/api/v1/suedtirolguestpass/cardtypes",
+                            type = "suedtirolguestpass.cardtypes",
+                            license = "open",
+                            rawformat = "json"
+                        });
+        }
 
-        //private async Task<Tuple<int, int>> DeleteOrDisableData(string id, bool delete)
-        //{
-        //    var deleteresult = 0;
-        //    var updateresult = 0;
+        private async Task<Tuple<int, int>> DeleteOrDisableData(string id, bool delete)
+        {
+            var deleteresult = 0;
+            var updateresult = 0;
 
-        //    if (delete)
-        //    {
-        //        deleteresult = await QueryFactory.Query("smgpois").Where("id", id)
-        //            .DeleteAsync();
-        //    }
-        //    else
-        //    {
-        //        var query =
-        //       QueryFactory.Query("smgpois")
-        //           .Select("data")
-        //           .Where("id", id);
+            if (delete)
+            {
+                deleteresult = await QueryFactory.Query("tags").Where("id", id)
+                    .DeleteAsync();
+            }
+            else
+            {
+                var query =
+               QueryFactory.Query("tags")
+                   .Select("data")
+                   .Where("id", id);
 
-        //        var data = await query.GetObjectSingleAsync<ODHActivityPoiLinked>();
+                var data = await query.GetObjectSingleAsync<TagLinked>();
 
-        //        if (data != null)
-        //        {
-        //            if (data.Active != false || data.SmgActive != false)
-        //            {
-        //                data.Active = false;
-        //                data.SmgActive = false;
+                if (data != null)
+                {
+                    if (data.Active != false)
+                    {
+                        data.Active = false;
+                        
+                        updateresult = await QueryFactory.Query("tags").Where("id", id)
+                                        .UpdateAsync(new JsonBData() { id = id, data = new JsonRaw(data) });
+                    }
+                }
+            }
 
-        //                updateresult = await QueryFactory.Query("smgpois").Where("id", id)
-        //                                .UpdateAsync(new JsonBData() { id = id, data = new JsonRaw(data) });
-        //            }
-        //        }
-        //    }
+            return Tuple.Create(updateresult, deleteresult);
+        }
 
-        //    return Tuple.Create(updateresult, deleteresult);
-        //}
+        private async Task<List<string>> GetAllDataBySourceAndType(List<string> sourcelist, List<string> typelist)
+        {
 
+            var query =
+               QueryFactory.Query("tags")
+                   .Select("id")
+                   .SourceFilter_GeneratedColumn(sourcelist)
+                   .WhereRaw("gen_types @> array\\[$$\\]", String.Join(",", typelist));
 
+            var eventids = await query.GetAsync<string>();
+
+            return eventids.ToList();
+        }
     }
+
+    public class LTSGuestcard
+    {
+        public string rid { get; set; }
+        public DateTime lastUpdate { get; set; }
+        public IDictionary<string,string> name { get; set; }
+        public bool isActive { get; set; }
+    }
+    
+
 }
