13a08dec2e7173c26de687766055f14ca965e62c
RudiThoeni <r.thoeni@noi.bz.it>
2024-11-28 11:04:08 +0100

    adding documentation wiki links

--- OdhApiCore/Controllers/generic/DistinctController.cs
+++ OdhApiCore/Controllers/generic/DistinctController.cs
@@ -19,6 +19,9 @@ using System.Threading.Tasks;
 
 namespace OdhApiCore.Controllers
 {
+    /// <summary>
+    /// <a href="https://github.com/noi-techpark/odh-docs/wiki/Distinct-Search-in-ODH-Tourism-api" target="_blank">Wiki Distinct Api</a>
+    /// </summary>
     [EnableCors("CorsPolicy")]
     [NullStringParameterActionFilter]
     public class DistinctController : OdhController
@@ -31,13 +34,12 @@ namespace OdhApiCore.Controllers
         #region SWAGGER Exposed API
 
         /// <summary>
-        /// GET Generic Distinct search of fields
+        /// GET Generic Distinct search of fields 
         /// </summary>
         /// <param name="pagenumber">Pagenumber</param>
         /// <param name="pagesize">Elements per Page, (default:10)</param>
         /// <param name="seed">Seed '1 - 10' for Random Sorting, '0' generates a Random Seed, 'null' disables Random Sorting, (default:null)</param>
-        /// <param name="odhtype">Mandatory search trough Entities (metadata, accommodation, odhactivitypoi, event, webcam, measuringpoint, ltsactivity, ltspoi, ltsgastronomy, article ..... )</param>
-        /// <param name="type">Mandatory search trough Entities (metadata, accommodation, odhactivitypoi, event, webcam, measuringpoint, ltsactivity, ltspoi, ltsgastronomy, article ..... )</param>
+        /// <param name="type">Mandatory search trough Entities (metadata, accommodation, odhactivitypoi, event, webcam, measuringpoint, ltsactivity, ltspoi, ltsgastronomy, article ..... ) <a href="https://github.com/noi-techpark/odh-docs/wiki/Dataset-Type" target="_blank">Wiki Dataset Type</a></param>
         /// <param name="fields">Mandatory Select a field for the Distinct Query, example fields=Source, arrays are selected with a [*] example HasLanguage[*] / Features[*].Id  (Only one field supported). <a href="https://github.com/noi-techpark/odh-docs/wiki/Common-parameters%2C-fields%2C-language%2C-searchfilter%2C-removenullvalues%2C-updatefrom#fields" target="_blank">Wiki fields</a></param>
         /// <param name="rawfilter"><a href="https://github.com/noi-techpark/odh-docs/wiki/Using-rawfilter-and-rawsort-on-the-Tourism-Api#rawfilter" target="_blank">Wiki rawfilter</a></param>
         /// <param name="rawsort"><a href="https://github.com/noi-techpark/odh-docs/wiki/Using-rawfilter-and-rawsort-on-the-Tourism-Api#rawfilter" target="_blank">Wiki rawsort</a></param>
