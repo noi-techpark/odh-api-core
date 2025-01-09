186a58bf4563e1499b0d2bb6d10c7a82253f5889
RudiThoeni <r.thoeni@noi.bz.it>
2025-01-09 11:54:26 +0100

    adding sta import deactivate data

--- OdhApiImporter/Helpers/STA/STAImportHelper.cs
+++ OdhApiImporter/Helpers/STA/STAImportHelper.cs
@@ -37,10 +37,15 @@ namespace OdhApiImporter.Helpers
 
         private static async Task<string> ReadStringDataManual(HttpRequest request)
         {
+            //CSV has to be encoded in UTF8
             using (StreamReader reader = new StreamReader(request.Body, Encoding.UTF8))
             {
                 return await reader.ReadToEndAsync();
             }
+            //using (StreamReader reader = new StreamReader(request.Body, Encoding.Default))
+            //{
+            //    return await reader.ReadToEndAsync();
+            //}
         }
 
         public async Task<UpdateDetail> PostVendingPointsFromSTA(
@@ -72,97 +77,144 @@ namespace OdhApiImporter.Helpers
                 var deletecounter = 0;
                 var errorcounter = 0;
 
+                List<string> idlistspreadsheet = new List<string>();
+
                 //Import Each STA Vendingpoi to ODH
                 foreach (var vendingpoint in vendingpoints.records)
                 {
-                    //Parse to ODHActivityPoi
-                    var odhactivitypoi = STA.ParseSTAPois.ParseSTAVendingPointToODHActivityPoi(
-                        vendingpoint
-                    );
-
-                    if (odhactivitypoi != null)
+                    if (!String.IsNullOrEmpty(vendingpoint.STA_ID))
                     {
-                        //MetaData
-                        //odhactivitypoi._Meta = MetadataHelper.GetMetadataobject<ODHActivityPoiLinked>(odhactivitypoi, MetadataHelper.GetMetadataforOdhActivityPoi); //GetMetadata(data.Id, "odhactivitypoi", sourcemeta, data.LastChange);
-                        //LicenseInfo                                                                                                                                    //License
-                        odhactivitypoi.LicenseInfo = LicenseHelper.GetLicenseforOdhActivityPoi(
-                            odhactivitypoi
+                        //Parse to ODHActivityPoi
+                        var odhactivitypoi = STA.ParseSTAPois.ParseSTAVendingPointToODHActivityPoi(
+                            vendingpoint
                         );
 
-                        if (odhactivitypoi.GpsPoints.ContainsKey("position"))
+                        if (odhactivitypoi != null)
                         {
-                            //Get Nearest District
-                            var geosearchresult = Helper.GeoSearchHelper.GetPGGeoSearchResult(
-                                odhactivitypoi.GpsPoints["position"].Latitude,
-                                odhactivitypoi.GpsPoints["position"].Longitude,
-                                10000
-                            );
-                            var nearestdistrict = await LocationInfoHelper.GetNearestDistrict(
-                                QueryFactory,
-                                geosearchresult,
-                                1
+                            //MetaData
+                            //odhactivitypoi._Meta = MetadataHelper.GetMetadataobject<ODHActivityPoiLinked>(odhactivitypoi, MetadataHelper.GetMetadataforOdhActivityPoi); //GetMetadata(data.Id, "odhactivitypoi", sourcemeta, data.LastChange);
+                            //LicenseInfo                                                                                                                                    //License
+                            odhactivitypoi.LicenseInfo = LicenseHelper.GetLicenseforOdhActivityPoi(
+                                odhactivitypoi
                             );
 
-                            if (nearestdistrict != null && nearestdistrict.Count() > 0)
+                            if (odhactivitypoi.GpsPoints.ContainsKey("position"))
                             {
-                                //Get LocationInfo Object
-                                var locationinfo =
-                                    await LocationInfoHelper.GetTheLocationInfoDistrict(
-                                        QueryFactory,
-                                        nearestdistrict.FirstOrDefault()?.Id
-                                    );
+                                //Get Nearest District
+                                var geosearchresult = Helper.GeoSearchHelper.GetPGGeoSearchResult(
+                                    odhactivitypoi.GpsPoints["position"].Latitude,
+                                    odhactivitypoi.GpsPoints["position"].Longitude,
+                                    10000
+                                );
+                                var nearestdistrict = await LocationInfoHelper.GetNearestDistrict(
+                                    QueryFactory,
+                                    geosearchresult,
+                                    1
+                                );
 
-                                if (locationinfo != null)
-                                    odhactivitypoi.LocationInfo = locationinfo;
+                                if (nearestdistrict != null && nearestdistrict.Count() > 0)
+                                {
+                                    //Get LocationInfo Object
+                                    var locationinfo =
+                                        await LocationInfoHelper.GetTheLocationInfoDistrict(
+                                            QueryFactory,
+                                            nearestdistrict.FirstOrDefault()?.Id
+                                        );
+
+                                    if (locationinfo != null)
+                                        odhactivitypoi.LocationInfo = locationinfo;
+                                }
                             }
-                        }
 
-                        //Adding TypeInfo Additional
-                        odhactivitypoi.AdditionalPoiInfos =
-                            await GetAdditionalTypeInfo.GetAdditionalTypeInfoForPoi(
-                                QueryFactory,
-                                odhactivitypoi?.SubType,
-                                new List<string>() { "de", "it", "en" }
-                            );
+                            //Adding TypeInfo Additional
+                            odhactivitypoi.AdditionalPoiInfos =
+                                await GetAdditionalTypeInfo.GetAdditionalTypeInfoForPoi(
+                                    QueryFactory,
+                                    odhactivitypoi?.SubType,
+                                    new List<string>() { "de", "it", "en" }
+                                );
 
-                        if (odhactivitypoi is { })
-                        {
-                            ODHTagHelper.SetMainCategorizationForODHActivityPoi(odhactivitypoi);
+                            if (odhactivitypoi is { })
+                            {
+                                ODHTagHelper.SetMainCategorizationForODHActivityPoi(odhactivitypoi);
 
-                            //Special get all Taglist and traduce it on import
-                            await GenericTaggingHelper.AddTagsToODHActivityPoi(
-                                odhactivitypoi,
-                                settings.JsonConfig.Jsondir
-                            );
-                            odhactivitypoi.TagIds =
-                                odhactivitypoi.Tags != null
-                                    ? odhactivitypoi.Tags.Select(x => x.Id).ToList()
-                                    : null;
+                                //Special get all Taglist and traduce it on import
+                                await GenericTaggingHelper.AddTagsToODHActivityPoi(
+                                    odhactivitypoi,
+                                    settings.JsonConfig.Jsondir
+                                );
+                                odhactivitypoi.TagIds =
+                                    odhactivitypoi.Tags != null
+                                        ? odhactivitypoi.Tags.Select(x => x.Id).ToList()
+                                        : null;
 
-                            //Save to Rawdatatable
-                            var rawdataid = await InsertInRawDataDB(vendingpoint);
+                                //Save to Rawdatatable
+                                var rawdataid = await InsertInRawDataDB(vendingpoint);
 
-                            //PublishedOn Info??
+                                //PublishedOn Info??
 
-                            //Save to PG
-                            //Check if data exists
-                            var result = await QueryFactory.UpsertData(
-                                odhactivitypoi,
-                                "smgpois",
-                                rawdataid,
-                                "sta.vendingpoint.import",
-                                importerURL
-                            );
+                                //Save to PG
+                                //Check if data exists
+                                var result = await QueryFactory.UpsertData(
+                                    odhactivitypoi,
+                                    "smgpois",
+                                    rawdataid,
+                                    "sta.vendingpoint.import",
+                                    importerURL
+                                );
 
-                            if (result.updated != null)
-                                updatecounter = updatecounter + result.updated.Value;
-                            if (result.created != null)
-                                newcounter = newcounter + result.created.Value;
-                            if (result.deleted != null)
-                                deletecounter = deletecounter + result.deleted.Value;
+                                idlistspreadsheet.Add(odhactivitypoi.Id);
+
+                                if (result.updated != null)
+                                    updatecounter = updatecounter + result.updated.Value;
+                                if (result.created != null)
+                                    newcounter = newcounter + result.created.Value;
+                                if (result.deleted != null)
+                                    deletecounter = deletecounter + result.deleted.Value;
+                            }
                         }
                     }
                 }
+
+                //Set all Deleted Vendingpoints to inactive
+                var idlistdb = await GetAllVendingpoints();
+                var idstodelete = idlistdb.Where(p => !idlistspreadsheet.Any(p2 => p2 == p));
+
+                foreach (var idtodelete in idstodelete)
+                {
+                    var deletedisableresult = await DeleteOrDisableData(idtodelete, false);
+
+                    if (deletedisableresult.Item1 > 0)
+                        WriteLog.LogToConsole(
+                            idtodelete,
+                            "dataimport",
+                            "sta.vendingpoint.import.deactivate",
+                            new ImportLog()
+                            {
+                                sourceid = idtodelete,
+                                sourceinterface = "sta.vendingpoint",
+                                success = true,
+                                error = "",
+                            }
+                        );
+                    else if (deletedisableresult.Item2 > 0)
+                        WriteLog.LogToConsole(
+                            idtodelete,
+                            "dataimport",
+                            "sta.vendingpoint.import.delete",
+                            new ImportLog()
+                            {
+                                sourceid = idtodelete,
+                                sourceinterface = "sta.vendingpoint",
+                                success = true,
+                                error = "",
+                            }
+                        );
+
+                    deletecounter =
+                        deletecounter + deletedisableresult.Item1 + deletedisableresult.Item2;
+                }
+
 
                 return new UpdateDetail()
                 {
@@ -195,5 +247,58 @@ namespace OdhApiImporter.Helpers
                 }
             );
         }
+
+        private async Task<List<string>> GetAllVendingpoints()
+        {
+            var query = QueryFactory
+                .Query("smgpois")
+                .Select("id")
+                .SourceFilter_GeneratedColumn(new List<string>() { "sta" })
+                .WhereLike("id", "salespoint_sta%");
+
+            var eventids = await query.GetAsync<string>();
+
+            return eventids.ToList();
+        }
+
+        private async Task<Tuple<int, int>> DeleteOrDisableData(string id, bool delete)
+        {
+            var deleteresult = 0;
+            var updateresult = 0;
+
+            if (delete)
+            {
+                deleteresult = await QueryFactory
+                    .Query("smgpois")
+                    .Where("id", id)
+                    .DeleteAsync();
+            }
+            else
+            {
+                var query = QueryFactory.Query("smgpois").Select("data").Where("id", id);
+
+                var data = await query.GetObjectSingleAsync<ODHActivityPoiLinked>();
+
+                if (data != null)
+                {
+                    if (data.Active != false)
+                    {
+                        data.Active = false;
+                        data.SmgActive = false;
+
+                        updateresult = await QueryFactory
+                            .Query("smgpois")
+                            .Where("id", id)
+                            .UpdateAsync(
+                                new JsonBData() { id = id, data = new JsonRaw(data) }
+                            );
+                    }
+                }
+            }
+
+            return Tuple.Create(updateresult, deleteresult);
+        }
+
+
     }
 }
